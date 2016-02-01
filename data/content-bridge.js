2591daa5a8e5c6b9dc44cdd6d8168dd2a0718e77
Olivier Yiptong <olivier@olivieryiptong.com>
2016-02-01 14:02:30 -0500

    initial activity-stream addon

---
+++ data/content-bridge.js
@@ -0,0 +1,20 @@
+/* globals unsafeWindow, cloneInto */
+
+"use strict";
+
+unsafeWindow.navigator.activity_streams_addon = true;
+
+window.addEventListener('content-to-addon', function(event) {
+  self.port.emit('content-to-addon', JSON.parse(event.detail));
+}, false);
+
+self.port.on('addon-to-content', function(data) {
+  const clonedData = cloneInto(data, document.defaultView);
+  window.dispatchEvent(
+    new CustomEvent('addon-to-content', {detail: clonedData})
+  );
+});
+
+window.addEventListener('pagehide', function() {
+  self.port.emit('content-to-addon', {type: 'pagehide'});
+}, false);
