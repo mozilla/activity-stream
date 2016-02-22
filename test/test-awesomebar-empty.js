6081566c5d379c29f3e5486e886ba359c8db0733
Ricky Rosario <rickyrosario@gmail.com>
2016-02-22 18:39:13 -0500

    fix(addon): #174 Fix this this issue

--- test/test-awesomebar-empty.js
+++ test/test-awesomebar-empty.js
@@ -3,6 +3,8 @@
 
 const test = require("sdk/test");
 const tabs = require("sdk/tabs");
+const windows = require("sdk/windows").browserWindows;
+const {viewFor} = require("sdk/view/core");
 const {ActivityStreams} = require("lib/ActivityStreams");
 const httpd = require("./lib/httpd");
 const {doGetFile} = require("./lib/utils");
@@ -29,6 +31,29 @@ exports["test awesomebar is empty for all app urls"] = function*(assert) {
         let browserWindow = windowMediator.getMostRecentWindow("navigator:browser");
         assert.equal(browserWindow.gURLBar.value, "");
         tab.close(resolve);
+      }
+    }));
+  }
+
+  app.unload();
+  yield new Promise(resolve => {
+    srv.stop(resolve);
+  });
+};
+
+exports["test awesomebar is empty for all app urls in new windows too"] = function*(assert) {
+  let path = "/dummy-activitystreams.html";
+  let url = `http://localhost:${PORT}${path}`;
+  let srv = httpd.startServerAsync(PORT, null, doGetFile("test/resources"));
+  let app = new ActivityStreams({pageURL: url});
+
+  for (let appURL of app.appURLs) {
+    yield new Promise(resolve => windows.open({
+      url: appURL,
+      onOpen: (window) => {
+        let chromeWindow = viewFor(window);
+        assert.equal(chromeWindow.gURLBar.value, "");
+        window.close(resolve);
       }
     }));
   }
