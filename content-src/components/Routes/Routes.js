ac34ae6114e7f161d93f2b89758bd757f3f5f787
Ricky Rosario <rickyrosario@gmail.com>
2016-03-01 17:34:59 -0500

    fix(addon): #223 hide app URLs in all cases

--- content-src/components/Routes/Routes.js
+++ content-src/components/Routes/Routes.js
@@ -1,10 +1,20 @@
 const React = require("react");
 const {Router, Route, IndexRoute} = require("react-router");
 const {createHashHistory} = require("history");
+const {connect} = require("react-redux");
+const {actions} = require("actions/action-manager");
 
 const history = createHashHistory({queryKey: false});
 
 const Routes = React.createClass({
+  componentDidMount() {
+    this.unlisten = history.listen(location => {
+      this.props.dispatch(actions.NotifyRouteChange(location));
+    });
+  },
+  componentDidUnmount() {
+    this.unlisten();
+  },
   render() {
     return (<Router history={history}>
       <Route path="/" component={require("components/Base/Base")}>
@@ -18,4 +28,4 @@ const Routes = React.createClass({
   }
 });
 
-module.exports = Routes;
+module.exports = connect(() => ({}))(Routes);
