b9afc554d6d046c94cb6956d4182bbc00ab55b78
Ricky Rosario <rickyrosario@gmail.com>
2016-03-03 12:51:01 -0500

    test

--- test/test-awesomebar-empty.js
+++ test/test-awesomebar-empty.js
@@ -5,6 +5,7 @@ const test = require("sdk/test");
 const tabs = require("sdk/tabs");
 const windows = require("sdk/windows").browserWindows;
 const {viewFor} = require("sdk/view/core");
+const {setTimeout} = require("sdk/timers");
 const {ActivityStreams} = require("lib/ActivityStreams");
 const httpd = require("./lib/httpd");
 const {doGetFile} = require("./lib/utils");
@@ -57,6 +58,34 @@ exports["test awesomebar is empty for all app urls in new windows too"] = functi
       }
     }));
   }
+
+  app.unload();
+  yield new Promise(resolve => {
+    srv.stop(resolve);
+  });
+};
+
+exports["test awesomebar is remains empty on hash changes"] = function*(assert) {
+  let path = "/dummy-activitystreams.html";
+  let url = `http://localhost:${PORT}${path}`;
+  let srv = httpd.startServerAsync(PORT, null, doGetFile("test/resources"));
+  let app = new ActivityStreams({pageURL: url});
+
+  yield new Promise(resolve => tabs.open({
+    url: url,
+    onReady: (tab) => {
+      let browserWindow = windowMediator.getMostRecentWindow("navigator:browser");
+      assert.equal(browserWindow.gURLBar.value, "");
+
+      // change the hash and verify url bar is still empty.
+      tab.url = url + "#/timeline";
+
+      setTimeout(() => {
+        assert.equal(browserWindow.gURLBar.value, "");
+        tab.close(resolve);
+      }, 10);
+    }
+  }));
 
   app.unload();
   yield new Promise(resolve => {
