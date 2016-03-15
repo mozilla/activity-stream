e8603119dd5e9676ce2c4b7094fa5a77b3978da5
Ursula Sarracini <ursulasarracini@gmail.com>
2016-03-15 13:18:45 -0400

    feat(addon): #258 Implement embedly client in the addon

--- content-src/lib/utils.js
+++ content-src/lib/utils.js
@@ -1,7 +1,3 @@
-const urlParse = require("url-parse");
-
-const ALLOWED_QUERY_PARAMS = new Set(["id", "p", "q", "query", "s", "search", "sitesearch"]);
-const REMOVE_KEYS = ["auth", "password", "username"];
 const RANDOM_COLORS = [
   [242, 88, 32], // orange
   [234, 56, 95], // magenta
@@ -43,37 +39,6 @@ module.exports = {
       return "";
     }
     return url.replace(/^((https?:)?\/\/)?(www\.)?/i, "").toLowerCase();
-  },
-
-  sanitizeUrl(site) {
-    if (!site) {
-      return "";
-    }
-
-    const parsedUrl = (typeof site === "string") ? urlParse(site, true) : site.parsedUrl;
-    const safeQueryParams = {};
-
-    if (!parsedUrl) {
-      return "";
-    }
-
-    // Remove any unwanted username/password from the parsed URL.
-    REMOVE_KEYS.forEach((key) => parsedUrl.set(key, ""));
-
-    // Filter out the allowed query params from the specified URL and update the
-    // query string.
-    Object.keys(parsedUrl.query)
-      .filter((param) => ALLOWED_QUERY_PARAMS.has(param))
-      .forEach((param) => safeQueryParams[param] = parsedUrl.query[param]);
-
-    parsedUrl.set("query", safeQueryParams);
-
-    // If there was not a specified protocol (ie: //mozilla.com), specify HTTPS.
-    if (!parsedUrl.protocol) {
-      parsedUrl.set("protocol", "https:");
-    }
-
-    return parsedUrl.toString();
   },
 
   getRandomFromTimestamp(percent, site) {
