e19098d01f33ef73a0eef3cd93b67926277f9bb6
k88hudson <k88hudson@gmail.com>
2016-04-18 18:06:09 -0400

    chore(debugger): Added ui component and data source tester

--- content-src/components/DebugPage/DebugPage.js
+++ content-src/components/DebugPage/DebugPage.js
@@ -1,6 +1,9 @@
 const React = require("react");
 const {connect} = require("react-redux");
-const {selectNewTabSites} = require("selectors/selectors");
+const {selectNewTabSites, selectSpotlight} = require("selectors/selectors");
+const {SpotlightItem} = require("components/Spotlight/Spotlight");
+const GroupedActivityFeed = require("components/ActivityFeed/ActivityFeed");
+const TopSites = require("components/TopSites/TopSites");
 
 // Only include this in DEVELOPMENT builds
 let JSONTree;
@@ -22,13 +25,61 @@ function Viewer(props) {
 }
 
 const DebugPage = React.createClass({
+  getInitialState() {
+    return {
+      component: "Spotlight",
+      dataSource: "Highlights"
+    };
+  },
   render() {
     const plainText = JSON.stringify({raw: this.props.raw, newTab: this.props.newTab}, null, 2);
     return (<main className="debug-page">
       <div className="new-tab-wrapper">
         <h2>Plain text</h2>
-        <textarea value={plainText} />
+        <textarea disabled value={plainText} />
         <Viewer {...this.props} />
+
+        <h2>UI tester</h2>
+        <div className="ui-tester">
+          <div className="form-group">
+            <label>UI Component</label>
+            <select value={this.state.component} onChange={e => this.setState({component: e.target.value})}>
+              <option value={"Spotlight"}>Highlight</option>
+              <option value={"TopSites"}>Top Sites</option>
+              <option value={"ActivityFeed"}>Activity Feed</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Data Source</label>
+            <select value={this.state.dataSource} onChange={e => this.setState({dataSource: e.target.value})}>
+              {Object.keys(this.props.raw).map(source => {
+                return (<option key={source} value={source}>{source}</option>);
+              })}
+            </select>
+          </div>
+        </div>
+        <div>
+          {this.state.component === "Spotlight" &&
+            <div className="spotlight">
+              {selectSpotlight({
+                FrecentHistory: this.props.raw[this.state.dataSource],
+                Blocked: {urls: new Set()}
+              }).rows.map(item => {
+                return (<SpotlightItem {...item} />);
+              })}
+            </div>
+          }
+          {this.state.component === "TopSites" &&
+            <TopSites
+              sites={this.props.raw[this.state.dataSource].rows}
+              length={this.props.raw[this.state.dataSource].rows.length} />
+          }
+          {this.state.component === "ActivityFeed" &&
+            <GroupedActivityFeed
+              sites={this.props.raw[this.state.dataSource].rows}
+              length={this.props.raw[this.state.dataSource].rows.length} />
+          }
+        </div>
       </div>
     </main>);
   }
