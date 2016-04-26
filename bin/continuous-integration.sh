daffd46431bb85167a2b3be5aea331d3d5d23bd6
Olivier Yiptong <olivier@olivieryiptong.com>
2016-04-26 14:57:39 -0400

    chore(ci): #442 continous integration deployments for master on travis CI

---
+++ bin/continuous-integration.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+if [ -z "$CONTINUOUS_INTEGRATION" ]
+then
+    set -x # sets bash debugging mode
+    echo "setting up development virtualenv"
+    export CFLAGS='-std=c99'
+fi
+
+fab deploy:dev_deploy=True || exit 1
