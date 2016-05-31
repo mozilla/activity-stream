7abc42a4d6efe957f427edf1aa5f7e29e0c668c5
k88hudson <k88hudson@gmail.com>
2016-05-31 13:10:00 -0400

    chore(addon): #781 use webpack in addon for external deps

---
+++ webpack.addon.config.js
@@ -0,0 +1,19 @@
+"use strict";
+const webpack = require("webpack");
+const absolute = (relPath) => require("path").join(__dirname, relPath);
+
+module.exports = {
+  entry: {
+    "vendor": absolute("addon-src/vendor.js")
+  },
+  output: {
+    path: absolute("lib"),
+    filename: "vendor.bundle.js"
+  },
+  module: {
+    loaders: [{test: /\.json$/, loader: "json"}]
+  },
+  plugins: [
+    new webpack.BannerPlugin("const platform_require = require; let platform_exports = exports;\n", {raw: true})
+  ]
+};
