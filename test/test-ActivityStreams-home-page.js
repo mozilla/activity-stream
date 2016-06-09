8ae085af48eded014c33f707293feb21497f4886
Ricky Rosario <rickyrosario@gmail.com>
2016-06-09 08:59:00 -0400

    keep track if user overrode the home page

--- test/test-ActivityStreams-home-page.js
+++ test/test-ActivityStreams-home-page.js
@@ -2,9 +2,11 @@
 
 const test = require("sdk/test");
 const prefService = require("sdk/preferences/service");
+const ss = require("sdk/simple-storage");
 const {ActivityStreams} = require("lib/ActivityStreams");
 
 exports["test activity stream loads on home page when appropriate"] = function*(assert) {
+  prefService.set("browser.startup.homepage", "about:home");
   let url = "http://foo.bar/baz";
   let app = new ActivityStreams({pageURL: url});
 
@@ -21,6 +23,16 @@ exports["test activity stream loads on home page when appropriate"] = function*(
   assert.equal("https://example.com", prefService.get("browser.startup.homepage"));
   app.unload();
   assert.equal("https://example.com", prefService.get("browser.startup.homepage"));
+
+  // If we override the pref and the user changes it back to about:home,
+  // ActivityStream shouldn't change it on next load.
+  prefService.set("browser.startup.homepage", "about:home");
+  ss.storage.homepageOverriden = true;
+  app = new ActivityStreams({pageURL: url});
+  assert.equal("about:home", prefService.get("browser.startup.homepage"));
+  app.unload();
+  assert.equal("about:home", prefService.get("browser.startup.homepage"));
+
 };
 
 test.run(exports);
