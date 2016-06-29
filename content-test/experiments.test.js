74b4a88ceeed769914d29f1e015db2369f1379b3
k88hudson <k88hudson@gmail.com>
2016-06-29 16:24:50 -0400

    add experiment test

---
+++ content-test/experiments.test.js
@@ -0,0 +1,53 @@
+const {assert} = require("chai");
+const definitions = require("../experiments.json");
+// This is the minimum size of the control group, expressed as a decimal
+const MIN_CONTROL = 0.2;
+
+describe("Experiment Definitions", () => {
+  let control = 1;
+  Object.keys(definitions).forEach(key => {
+    const experiment = definitions[key];
+    if (experiment.active !== false) {
+      control -= experiment.variant.threshold;
+    }
+    describe(`experiment: ${key}`, () => {
+      it("should have .name", () => {
+        assert.property(experiment, "name", "exists");
+        assert.isString(experiment.name);
+      });
+      it("should have .description", () => {
+        assert.property(experiment, "description", "exists");
+        assert.isString(experiment.description);
+      });
+      it("should have .control", () => {
+        assert.property(experiment, "control", "exists");
+        assert.isObject(experiment.control);
+      });
+      it("should have .control.value", () => {
+        assert.property(experiment.control, "value", "exists");
+      });
+      it("should have .control.description", () => {
+        assert.property(experiment.control, "description", "exists");
+        assert.isString(experiment.control.description);
+      });
+      it("should have .variant", () => {
+        assert.property(experiment, "variant", "exists");
+        assert.isObject(experiment.variant);
+      });
+      it("should have .variant.description", () => {
+        assert.property(experiment.variant, "description", "exists");
+        assert.isString(experiment.variant.description);
+      });
+      it("should have .variant.threshold", () => {
+        assert.property(experiment.variant, "threshold", "exists");
+        assert.isNumber(experiment.variant.threshold);
+        assert.isAtMost(experiment.variant.threshold, 1);
+        assert.isAtLeast(experiment.variant.threshold, 0);
+      });
+    });
+  });
+
+  it(`should have a control percentage of at least ${MIN_CONTROL}`, () => {
+    assert.isAtLeast(control, MIN_CONTROL, `control is at least ${MIN_CONTROL}`);
+  });
+});
