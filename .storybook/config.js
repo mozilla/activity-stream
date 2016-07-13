5b60beb9ea53b50e38bc3122011cd4760c2e2a1d
Dan Mosedale <dmose@meer.net>
2016-07-13 12:23:43 -0700

    Basically working

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
 const { configure } = require('@kadira/storybook');
 
+require("../data/content/main.css");
+
 function loadStories() {
   require('../content-test/components/Search.story');
   // require as many stories as you need.
