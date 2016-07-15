c49f5127f1aa065ed50cf033180b72a5e6376ee3
Dan Mosedale <dmose2@gmail.com>
2016-07-14 22:08:00 -0700

    Hack up a provider story

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,8 +3,9 @@ const {configure} = require('@kadira/storybook');
 require("../data/content/main.css");
 
 function loadStories() {
-  require('../content-test/components/Search.story');
+  require('../content-test/components/Provider.story');
   require('../content-test/components/ContextMenu.story');
+  require('../content-test/components/Search.story');
   // require as many stories as you need.
 }
 
