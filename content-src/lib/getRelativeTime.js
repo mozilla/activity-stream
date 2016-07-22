61d25552757c9048af60ecea87e8f36598257d72
Peter deHaan <peter@deseloper.com>
2016-07-22 15:34:29 -0700

    Remove JSCS and replace with ESLint rules

--- content-src/lib/getRelativeTime.js
+++ content-src/lib/getRelativeTime.js
@@ -6,17 +6,17 @@ moment.updateLocale("en", {
   relativeTime: {
     future: "in %s",
     past: "%s",
-    s:  "<1m",
-    m:  "%dm",
-    mm:  "%dm",
-    h:  "%dh",
-    hh:  "%dh",
-    d:  "%dd",
-    dd:  "%dd",
-    M:  "%dM",
-    MM:  "%dM",
-    y:  "%dy",
-    yy:  "%dy"
+    s: "<1m",
+    m: "%dm",
+    mm: "%dm",
+    h: "%dh",
+    hh: "%dh",
+    d: "%dd",
+    dd: "%dd",
+    M: "%dM",
+    MM: "%dM",
+    y: "%dy",
+    yy: "%dy"
   }
 });
 
