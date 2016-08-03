966d0e1c84bd393c949d6e138f995082144ecae8
Dan Mosedale <dmose2@gmail.com>
2016-08-03 14:11:01 -0700

    Remove Search and Provider examples

--- content-test/components/Search.story.js
+++
@@ -1,9 +0,0 @@
-const React = require("react");
-const {storiesOf} = require("@kadira/storybook");
-const {Search} = require("../../content-src/components/Search/Search");
-const SearchData = require("lib/fake-data.js").Search;
-
-storiesOf("Search", module)
-  .add("search box", () => (
-    <Search {...SearchData} dispatch={() => {}}/>
-  ));
