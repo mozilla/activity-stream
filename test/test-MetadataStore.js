9f79483a181ba4b14b3ed4d677c669c456deba38
Ed Lee <edilee@mozilla.com>
2016-08-04 14:10:50 -0700

    chore(lint): Enable no-sequences.

--- test/test-MetadataStore.js
+++ test/test-MetadataStore.js
@@ -216,7 +216,7 @@ exports.test_on_an_invalid_connection = function*(assert) {
   let cacheKeys = metadataFixture.map(fixture => {return fixture.cache_key;});
   let metaObjects = yield gMetadataStore.asyncGetMetadataByCacheKey(cacheKeys);
   assert.equal(metaObjects.length, 0, "It should return an empty array if the connection is closed or not established");
-},
+};
 
 exports.test_color_conversions = function*(assert) {
   const white = [0, 0, 0];
