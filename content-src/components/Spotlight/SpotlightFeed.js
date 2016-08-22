21a28d67465357713000782ab87bcda7dd885ee5
Andrei Oprea <andrei.br92@gmail.com>
2016-08-22 16:54:25 -0700

    New design for grouped highlights

---
+++ content-src/components/Spotlight/SpotlightFeed.js
@@ -0,0 +1,87 @@
+const React = require("react");
+const {connect} = require("react-redux");
+const {justDispatch} = require("selectors/selectors");
+const getHighlightContextFromSite = require("selectors/getHighlightContextFromSite");
+const {actions} = require("common/action-manager");
+const SiteIcon = require("components/SiteIcon/SiteIcon");
+const LinkMenu = require("components/LinkMenu/LinkMenu");
+const LinkMenuButton = require("components/LinkMenuButton/LinkMenuButton");
+const HighlightContext = require("components/HighlightContext/HighlightContext");
+const classNames = require("classnames");
+const {prettyUrl, getRandomFromTimestamp} = require("lib/utils");
+const moment = require("moment");
+
+const ICON_SIZE = 20;
+const TOP_LEFT_ICON_SIZE = 32;
+
+class SpotlightFeedItem extends React.Component {
+  _updateWeights(index) {
+    return () => {
+      this.props.dispatch(actions.NotifyUpdateWeights(index));
+
+      this.props.dispatch(actions.NotifyEvent({
+        event: "CLICK",
+        page: "NEW_TAB",
+        source: "ACTIVITY_FEED",
+        action_position: index
+      }));
+    }
+  }
+
+  render() {
+    const props = this.props.site;
+    const dateLabel = moment(props.lastVisitDate).fromNow() + " ago";
+    let icon;
+    const iconProps = {
+      ref: "icon",
+      className: "feed-icon",
+      site: props,
+      faviconSize: ICON_SIZE,
+    };
+
+    if (props.images && props.images[0]) {
+      icon = (<div className="feed-icon-image" style={{backgroundImage: `url(${props.images[0].url})`}}>
+        <SiteIcon {...iconProps} width={TOP_LEFT_ICON_SIZE} height={TOP_LEFT_ICON_SIZE} />
+      </div>);
+    } else {
+      icon = (<SiteIcon {...iconProps} />);
+    }
+
+    return <li className="feed-item">
+      <a onClick={this._updateWeights(this.props.index)} href={props.url}>
+        {icon}
+        <div className="feed-details">
+          <div className="feed-description">
+            <h4 className="feed-title" ref="title">{props.title || props.url}</h4>
+            <div className="feed-summary">
+              <span className="feed-url" ref="url" data-feed-url={prettyUrl(props.provider_display)} />
+              <span className="feed-summary-text">{props.description}</span>
+            </div>
+            {this.props.preview && <MediaPreview previewInfo={this.props.preview} />}
+          </div>
+          <div className="feed-stats">
+            <div className="feed-source"></div>
+            <div ref="lastVisit" className="last-visit" data-last-visit={dateLabel} />
+          </div>
+        </div>
+      </a>
+    </li>;
+  }
+}
+
+class SpotlightFeed extends React.Component {
+  _renderItem(site, i, dispatch) {
+    return <SpotlightFeedItem site={site} key={i} index={i} dispatch={dispatch} />
+  }
+
+  render() {
+    return <div className="grouped-highlight-feed">
+      <ul className="activity-feed">
+        {this.props.sites.map((site, i) => this._renderItem(site, i, this.props.dispatch))}
+      </ul>
+    </div>;
+  }
+}
+
+module.exports = connect(justDispatch)(SpotlightFeed);
+
