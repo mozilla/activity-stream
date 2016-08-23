fcea5bd79c5cf1befe82affd152c9077bab03938
k88hudson <k88hudson@gmail.com>
2016-08-23 12:42:15 -0400

    chore(tests): #1082 Update PrefsProvider tests

--- test/test-PrefsProvider.js
+++ test/test-PrefsProvider.js
@@ -1,18 +1,11 @@
 const test = require("sdk/test");
-const {PrefsProvider} = require("addon/PrefsProvider");
 const {getTestActivityStream} = require("./lib/utils");
-const simplePrefs = require("sdk/simple-prefs");
 
 const as = getTestActivityStream();
 
 exports["test PrefsProvider instance and initializes"] = assert => {
   assert.ok(as._prefsProvider, "has .prefsProvider");
-  assert.ok(as._prefsProvider instanceof PrefsProvider, "is a PrefsProvider instance");
   assert.ok(as._prefsProvider.onPrefChange, "onPrefChange listener was addded");
-};
-
-exports["test PrefsProvider has the right simplePrefs"] = assert => {
-  assert.equal(as._prefsProvider.simplePrefs, simplePrefs);
 };
 
 exports["test PrefsProvider unloads"] = assert => {
