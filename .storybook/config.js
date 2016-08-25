a46c3f72c06096464dbb3128a217a3982070e21f
Dan Mosedale <dmose2@gmail.com>
2016-08-25 08:41:10 -0700

    chore(storybook): support auto-building of storybooks for PR commits by storybook.io

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,21 @@
 const {configure} = require("@kadira/storybook");
 
+/**
+ * Static storybook builds (i.e. those used by storybooks.io) need a slightly
+ * different path for their favicons.
+ *
+ * @type {String}
+ */
+let staticPrefix = "";
+
+// Note that process.env.STORYBOOK_* variables are injected by storybook
+// via webpack.
+if (process.env.STORYBOOK_STATIC) {
+  staticPrefix = "content/";
+}
+let stylesheetLink = document.getElementById("main-stylesheet");
+stylesheetLink.setAttribute("href", `${staticPrefix}main.css`);
+
 function loadStories() {
   require("../content-test/components/Spotlight.story");
   require("../content-test/components/ContextMenu.story");
--- content-test/components/Spotlight.story.js
+++ content-test/components/Spotlight.story.js
@@ -2,8 +2,23 @@ const React = require("react");
 const {SpotlightItem} = require("components/Spotlight/Spotlight");
 const {storiesOf, action} = require("@kadira/storybook");
 const {createMockProvider} = require("../test-utils");
-const {createSite} = require("../faker");
+const faker = require("../faker");
+const createSite = faker.createSite;
 const Provider = createMockProvider({dispatch: action("dispatched a redux action")});
+
+// We have to set up both faker slightly differently if we're building
+// statically (eg for storybooks.io).  In theory, we should need to run
+// the faker thing from any story file that uses faker, but for whatever
+// reason, that doesn't seem to actually be true.  I'd guess that webpack
+// somehow optimizes things so that there's only a single copy of faker
+// in memory at once.  Interestingly, putting this code in config.js doesn't
+// work at all.
+
+// Note that process.env.STORYBOOK_* variables are injected by storybook
+// via webpack.
+if (process.env.STORYBOOK_STATIC) {
+  faker.base_tip_top_favicon_prefix = "content/";
+}
 
 // XXX should get rid of container here.  See comment in ContextMenu.story
 // for details.
