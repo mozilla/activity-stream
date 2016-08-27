8b4a1fdd92686549abd551d7da62a0d33956ad6f
Andrei Oprea <andrei.br92@gmail.com>
2016-08-26 17:30:37 -0700

    Add more tests

--- common/recommender/Baseline.js
+++ common/recommender/Baseline.js
@@ -1,9 +1,8 @@
 "use strict";
 
 const URL = require("../../addon/vendor.bundle.js").urlParse;
-const {INFINITE_SCROLL_THRESHOLD} = require("../constants");
 const getBestImage = require("../getBestImage");
-const COEFFICIENTS = [0.4, 0.7, -0.2, 0.1, -0.5, -0.3, -0.8, -0.6, -0.7];
+const COEFFICIENTS = [0.1, 0.5, -0.2, 0.1, -0.2, -0.3, -0.6, -0.7];
 
 /**
  * Score function for URLs.
@@ -16,14 +15,16 @@ class Baseline {
   constructor(history, options = {}) {
     this.domainCounts = history.reduce(this.countDomainOccurrences, new Map());
     this.options = options;
-    this.features = {
+    // Features that are extracted from URLs and need normalization.
+    // Key 0 holds the min, key 1 holds max, using arrays for brevity.
+    this.normalizeFeatures = {
       idf: [1, 0],
       imageCount: [1, 0],
       description: [1, 0],
       pathLength: [1, 0],
       largestImage: [1, 0],
       queryLength: [1, 0]
-    }
+    };
   }
 
   extractFeatures(entry) {
@@ -35,10 +36,10 @@ class Baseline {
     const tf = Math.max(entry.visitCount, 1);
     const idf = Math.log(this.domainCounts.size / occurences);
     const imageCount = entry.images ? entry.images.length : 0;
-    const isBookmarked = entry.bookmarkId > 0 ? 1 : 0;
-    const description = entry.description ? entry.description.length : 0;
+    const isBookmarked = entry.bookmarkId || 0;
+    const description = this.extractDescriptionLength(entry);
     const pathLength = urlObj.pathname.split("/").filter(e => e.length).length;
-    const largestImage = this.extractLargestImage(entry);
+    const largestImage = this.extractLargestImage(entry.images);
     const queryLength = urlObj.query.length;
 
     const features = {age, tf, idf, imageCount, isBookmarked, description, pathLength, queryLength, largestImage};
@@ -47,40 +48,101 @@ class Baseline {
     return Object.assign({}, entry, {features, host});
   }
 
-  normalizeFeatures(features) {
-    const normalizeKeys = Object.keys(this.features);
+  normalize(features) {
+    let delta;
+    const normalizeKeys = Object.keys(this.normalizeFeatures);
+
     normalizeKeys.forEach(key => {
-      if (this.features[key][1] !== this.features[key][0]) { // No division by 0.
-        features[key] = (features[key] - this.features[key][0]) / (this.features[key][1] - this.features[key][0]);
+      if (this.normalizeFeatures[key][1] !== this.normalizeFeatures[key][0]) { // No division by 0.
+        delta = this.normalizeFeatures[key][1] - this.normalizeFeatures[key][0];
+        features[key] = (features[key] - this.normalizeFeatures[key][0]) / delta;
       }
     });
   }
 
   scoreEntry(entry) {
-    this.normalizeFeatures(entry.features);
-    const {age, tf, idf, queryLength, imageCount, pathLength, isBookmarked, description, largestImage} = entry.features;
+    this.normalize(entry.features);
 
+    const {age, tf, idf, queryLength, imageCount, pathLength, description, largestImage} = entry.features;
     let score = this.decay(tf * idf, // Score
-      // Features: Age in hours, number of visits to url, url query length, number of images, is a bookmark,
-      //           has description.
-      [age, tf, idf, queryLength, imageCount, pathLength, isBookmarked, description, largestImage],
+      // Features: Age in hours, number of visits to url, how often you go to the domain, number of images,
+      //           length of path, length of description, size of biggest image.
+      [age, tf, idf, queryLength, imageCount, pathLength, description, largestImage],
       // Features weights: Positive values decrease the score proportional to a factor of feature * weight.
       //                   Negative values increase score proportional to a factor of feature * weight.
       this.options.highlightsCoefficients || COEFFICIENTS);
 
-    if (!imageCount || !largestImage) {
-      score *= 0.8;
-    }
+    score = this.adjustScore(score, entry.features);
 
     return Object.assign({}, entry, {score});
   }
 
+  /**
+   * Extra penalty/reward we want to adjust the score by.
+   *
+   * @param {Number} score - initial value.
+   * @param {Object} features - associated features and their values.
+   */
+  adjustScore(score, features) {
+    let newscore = score;
+
+    if (!features.imageCount || !features.largestImage) {
+      newscore *= 0.5;
+    }
+
+    if (!features.description) {
+      newscore *= 0.5;
+    }
+
+    if (features.isBookmarked) {
+      newscore *= 1.8;
+    }
+
+    return newscore;
+  }
+
+  extractDescriptionLength(entry) {
+    if (!entry.description ||
+        entry.title === entry.description ||
+        entry.url === entry.description) {
+      return 0;
+    }
+
+    return entry.description.length;
+  }
+
+  /**
+   * Update min and max values of the features that require normalization.
+   *
+   * @param {Object} newFeatures - features and associated values.
+   */
   updateFeatureMinMax(newFeatures) {
-    const keys = Object.keys(this.features);
+    const keys = Object.keys(this.normalizeFeatures);
     keys.forEach(key => {
-      this.features[key][0] = Math.min(this.features[key][0], newFeatures[key]);
-      this.features[key][1] = Math.max(this.features[key][1], newFeatures[key]);
+      this.normalizeFeatures[key][0] = this.selectMinValue(this.normalizeFeatures[key][0], newFeatures[key]);
+      this.normalizeFeatures[key][1] = this.selectMaxValue(this.normalizeFeatures[key][1], newFeatures[key]);
     });
+  }
+
+  /**
+   * Guard against undefined values that cause Math.{min, max} to return NaN.
+   */
+  selectMaxValue(oldv, newv) {
+    const value = Math.max(oldv, newv);
+    if (Number.isNaN(value)) {
+      return oldv;
+    }
+
+    return value;
+  }
+
+  selectMinValue(oldv, newv) {
+    const value = Math.min(oldv, newv);
+    if (Number.isNaN(value)) {
+      return oldv;
+    }
+
+    return value;
   }
 
   updateOptions(options) {
@@ -98,6 +160,7 @@ class Baseline {
       .map(entry => this.scoreEntry(entry))
       .sort(this.sortDescByScore);
 
+    // Decreases score for consecutive items from the same host.
     results = this.dedupe(results);
 
     return results.sort(this.sortDescByScore);
@@ -122,10 +185,11 @@ class Baseline {
   }
 
   /**
-   * XXX
+   * Return the size of the largest image.
+   * Assumes `getBestImage` returns largest image.
    */
-  extractLargestImage(entry) {
-    const image = getBestImage(entry);
+  extractLargestImage(images) {
+    const image = getBestImage(images);
     if (!image) {
       return 0;
     }
@@ -153,7 +217,7 @@ class Baseline {
    * @returns {Number}
    */
   decay(value, e, c) {
-    let exp = e.reduce((acc, v, i) => acc + v * c[i], 0);
+    let exp = e.reduce((acc, v, i) => acc + (v || 0) * c[i], 0);
 
     return value * Math.pow(Math.E, -exp);
   }
@@ -162,10 +226,17 @@ class Baseline {
     return b.score - a.score;
   }
 
-  dedupe(arr) {
+  /**
+   *  Decrease the score for consecutive items from the same domain.
+   *  Combined with sorting by score the result is we don't see consecutive
+   *  items from the same domain.
+   *
+   *  @param {Array} entries - scored and sorted highlight items.
+   */
+  dedupe(entries) {
     let penalty = 0.8;
 
-    return arr.map((e, i, arr) => {
+    return entries.map((e, i, arr) => {
       if (i > 0 && arr[i - 1].host === e.host) {
         let score = e.score * penalty;
         penalty -= 0.2;
@@ -176,7 +247,6 @@ class Baseline {
       return e;
     });
   }
-
 }
 
 exports.Baseline = Baseline;
