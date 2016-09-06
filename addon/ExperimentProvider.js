da1fddff5e401867d45b0735b1a09769fb7cf997
k88hudson <k88hudson@gmail.com>
2016-09-06 15:58:46 -0400

    feat(experiments): #964 Add experiment overriding via prefs

--- addon/ExperimentProvider.js
+++ addon/ExperimentProvider.js
@@ -1,32 +1,60 @@
 const seedrandom = require("common/vendor")("seedrandom");
+const simplePrefs = require("sdk/simple-prefs");
+const OVERRIDE_PREF = "experimentOverrides";
 
+exports.OVERRIDE_PREF = OVERRIDE_PREF;
 exports.ExperimentProvider = class ExperimentProvider {
-  constructor(clientID, experiments = require("experiments.json"), rng) {
+  constructor(clientID, experiments = require("../experiments.json"), rng) {
     this._clientID = clientID;
+    this._experiments = experiments;
     this._rng = rng || seedrandom(clientID);
     this._data = {};
     this._experimentId = null;
+  }
+
+  init() {
+    this.setValues();
+    this._onPrefChange = () => this.setValues();
+    simplePrefs.on(OVERRIDE_PREF, this._onPrefChange);
+  }
+
+  setValues() {
+    // Load override pref, if defined
+    const overrides = simplePrefs.prefs[OVERRIDE_PREF] && simplePrefs.prefs[OVERRIDE_PREF].split(/,?\s+/);
 
     const randomNumber = this._rng();
     let floor = 0;
 
-    Object.keys(experiments).forEach(key => {
-      const experiment = experiments[key];
-      if (experiment.active === false) {
+    Object.keys(this._experiments).forEach(key => {
+      let inExperiment;
+      const experiment = this._experiments[key];
+      if (!overrides && experiment.active === false) {
         return;
       }
       const {variant, control} = experiment;
-      const ceiling = variant.threshold + floor;
-      if (ceiling > 1) {
-        throw new Error("Your variant cohort sizes should add up to less than 1.");
-      }
-      const inExperiment = randomNumber >= floor && randomNumber < ceiling;
-      if (inExperiment) {
-        this._experimentId = variant.id;
+
+      // If an override pref is defined, use it to determine which
+      // experiment values are used. If not, just use our random number generator.
+      // Note that no _experimentId is set for manually overridden prefs.
+      if (overrides) {
+        inExperiment = overrides.includes(key);
+      } else {
+        const ceiling = variant.threshold + floor;
+        if (ceiling > 1) {
+          throw new Error("Your variant cohort sizes should add up to less than 1.");
+        }
+        inExperiment = randomNumber >= floor && randomNumber < ceiling;
+        if (inExperiment) {
+          this._experimentId = variant.id;
+        }
+        floor = ceiling;
       }
       this._data[key] = inExperiment ? variant.value : control.value;
-      floor = ceiling;
     });
+
+    if (overrides) {
+      console.log(`The following experiments were turned on via overrides:\n - ${overrides.join("\n - ")}`); // eslint-disable-line no-console
+    }
   }
 
   // This is an object representing all experiments
@@ -38,5 +66,11 @@ exports.ExperimentProvider = class ExperimentProvider {
   // or an id indicating the experiment/variant if they are part of it.
   get experimentId() {
     return this._experimentId;
+  }
+
+  destroy() {
+    simplePrefs.removeListener(OVERRIDE_PREF, this._onPrefChange);
+    this._data = {};
+    this._experimentId = null;
   }
 };
