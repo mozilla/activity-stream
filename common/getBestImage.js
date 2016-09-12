ed2b7cfb7d890cde6f73bb651a70574424c87fc0
Ursula Sarracini <ursulasarracini@gmail.com>
2016-09-12 16:06:01 -0400

    fix(content): Closes #1149 Remove best image logic

--- common/getBestImage.js
+++ common/getBestImage.js
@@ -1,29 +1,7 @@
-const IMG_HEIGHT = 226;
-const IMG_WIDTH = 124;
-
 module.exports = function getBestImage(images) {
+  // we will always either get one, and consequently best image, or no image at all
   if (!images || !images.length) {
     return null;
   }
-  const filteredImages = images.filter(image => {
-    if (!image.url) {
-      return false;
-    }
-    if (!image.width || image.width < IMG_WIDTH) {
-      return false;
-    }
-    if (!image.height || image.height < IMG_HEIGHT) {
-      return false;
-    }
-    return true;
-  });
-
-  if (!filteredImages.length) {
-    return null;
-  }
-
-  return filteredImages[0];
+  return images[0];
 };
-
-module.exports.IMG_HEIGHT = IMG_HEIGHT;
-module.exports.IMG_WIDTH = IMG_WIDTH;
