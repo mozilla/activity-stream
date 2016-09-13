f8bda05ba72cde0805ab2161d210f61bb73fb744
Ed Lee <edilee@mozilla.com>
2016-09-13 13:02:31 -0700

    feat(content): Add dismiss button for timeline filtering

--- content-src/components/Header/Header.js
+++ content-src/components/Header/Header.js
@@ -13,7 +13,10 @@ const Header = React.createClass({
     };
   },
   getInitialState() {
-    return {showDropdown: false};
+    return {
+      filterQuery: "",
+      showDropdown: false
+    };
   },
   onClick() {
     if (this.props.disabled) {
@@ -21,8 +24,12 @@ const Header = React.createClass({
     }
     this.setState({showDropdown: !this.state.showDropdown});
   },
+  updateFilter(query) {
+    this.setState({filterQuery: query});
+    this.props.dispatch(actions.NotifyFilterQuery(query));
+  },
   render() {
-    const props = this.props;
+    const {props, state} = this;
     return (<header className="head">
 
       <section ref="clickElement" className={classNames("nav", {"disabled": props.disabled})} onClick={this.onClick}>
@@ -31,16 +38,21 @@ const Header = React.createClass({
           <span>{props.title}</span>
           <span ref="caret" hidden={props.disabled} className="arrow" />
         </h1>
-        <ul ref="dropdown" className="nav-picker" hidden={!this.state.showDropdown}>
+        <ul ref="dropdown" className="nav-picker" hidden={!state.showDropdown}>
           {props.links.map(link => <li key={link.to}><Link to={link.to}>{link.title}</Link></li>)}
         </ul>
       </section>
       <section className="filter">
         <input
-          onChange={e => props.dispatch(actions.NotifyFilterQuery(e.target.value))}
+          onChange={e => this.updateFilter(e.target.value)}
           placeholder="Search your Activity Stream"
           ref="filter"
-          type="search" />
+          type="search"
+          value={state.filterQuery} />
+        {state.filterQuery && <span
+          className="icon icon-dismiss"
+          onClick={() => this.updateFilter("")}
+          ref="filterDismiss" />}
       </section>
       <section className="user-info">
         {props.userName && <span>
