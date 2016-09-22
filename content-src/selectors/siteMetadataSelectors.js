b8e2cc47e04f442aa57bb0d1f0e65838e9d421e7
Ed Lee <edilee@mozilla.com>
2016-09-22 11:28:43 -0700

    feat(topsites): Show shorter top sites label without top level domain

--- content-src/selectors/siteMetadataSelectors.js
+++ content-src/selectors/siteMetadataSelectors.js
@@ -8,7 +8,12 @@ function selectSiteProperties(site) {
   const metadataFavicon = site.favicons && site.favicons[0] && site.favicons[0].url;
   const favicon = site.favicon_url || metadataFavicon || site.favicon;
   const parsedUrl = site.parsedUrl || urlParse(site.url || "");
-  const label = prettyUrl(parsedUrl.hostname);
+
+  // Remove the eTLD (e.g., com, net) and the preceding period from the hostname
+  const eTLDLength = (site.eTLD || "").length;
+  const eTLDExtra = eTLDLength > 0 ? -eTLDLength - 1 : Infinity;
+  const label = prettyUrl(parsedUrl.hostname).slice(0, eTLDExtra);
+
   return {favicon, parsedUrl, label};
 }
 
