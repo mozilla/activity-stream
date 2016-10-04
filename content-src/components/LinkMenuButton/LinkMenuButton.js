1d3ff0c7e9c6345044fd74a5a8cd321b151092cd
Ricky Rosario <rickyrosario@gmail.com>
2016-10-04 11:17:50 -0400

    oops, didnt mean to remove this

--- content-src/components/LinkMenuButton/LinkMenuButton.js
+++ content-src/components/LinkMenuButton/LinkMenuButton.js
@@ -3,6 +3,7 @@ const React = require("react");
 const LinkMenuButton = React.createClass({
   render() {
     return (<button className="link-menu-button" onClick={e => {
+      e.preventDefault();
       this.props.onClick(e);
     }}>
       <span className="sr-only">Open context menu</span>
