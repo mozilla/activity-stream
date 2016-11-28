8856daa22a124e6db9f06c1f52a5c58578e96d20
Ursula Sarracini <ursulasarracini@gmail.com>
2016-11-28 15:32:29 -0500

    fix(reporting): fix #1831 Missing TOP_SITES pings

--- content-src/components/TopSites/TopSites.js
+++ content-src/components/TopSites/TopSites.js
@@ -26,7 +26,7 @@ const TopSitesItem = React.createClass({
     const index = site.index;
     const isActive = this.state.showContextMenu && this.state.activeTile === index;
     return (<div className={classNames("tile-outer", {active: isActive})} key={site.guid || site.cache_key || index}>
-      <a onClick={() => this.onClick(index)} className="tile" href={site.url}>
+      <a onClick={() => this.props.onClick(index)} className="tile" href={site.url}>
         <SiteIcon className="tile-img-container" site={site} faviconSize={32} showTitle={true} />
         <div className="inner-border" />
       </a>
