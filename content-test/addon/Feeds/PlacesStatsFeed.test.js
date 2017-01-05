0ed91c32427f3a3ab4d39cf0e0ebf54c9f1419b7
Ursula Sarracini <ursulasarracini@gmail.com>
2017-01-05 10:03:32 -0500

    metrics(metadata): Closes #1897 Track metadata coverage for sites

--- content-test/addon/Feeds/PlacesStatsFeed.test.js
+++ content-test/addon/Feeds/PlacesStatsFeed.test.js
@@ -1,7 +1,8 @@
 const PlacesProvider = {
   links: {
     getHistorySize: sinon.spy(() => Promise.resolve(42)),
-    getBookmarksSize: sinon.spy(() => Promise.resolve(1))
+    getBookmarksSize: sinon.spy(() => Promise.resolve(1)),
+    getHistorySizeSince: sinon.spy(() => Promise.resolve(0))
   }
 };
 const {PlacesStatsUpdate} = require("common/action-manager").actions;
@@ -13,7 +14,12 @@ describe("PlacesStatsFeed", () => {
   beforeEach(() => {
     PlacesProvider.links.getHistorySize.reset();
     PlacesProvider.links.getBookmarksSize.reset();
-    instance = new PlacesStatsFeed();
+    PlacesProvider.links.getHistorySizeSince.reset();
+    const MetadataStore = {
+      asyncGetOldestInsert: sinon.spy(() => Promise.resolve(10)),
+      asyncCountAllItems: sinon.spy(() => Promise.resolve(0))
+    };
+    instance = new PlacesStatsFeed({metadataStore: MetadataStore});
   });
   it("should create a PlacesStatsFeed", () => {
     assert.instanceOf(instance, PlacesStatsFeed);
@@ -29,7 +35,11 @@ describe("PlacesStatsFeed", () => {
     });
     it("should resolve with a PlacesStatsUpdate action", () => instance.getData().then(action => {
       assert.isObject(action);
-      assert.deepEqual(action, PlacesStatsUpdate(42, 1));
+      assert.deepEqual(action, PlacesStatsUpdate(42, 1, 0));
+    }));
+    it("should get the appropriate data from the metadata store", () => instance.getData().then(() => {
+      assert.calledOnce(instance.options.metadataStore.asyncGetOldestInsert);
+      assert.calledOnce(instance.options.metadataStore.asyncCountAllItems);
     }));
   });
 
