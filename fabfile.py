12b303f703b0232bd1b2f56e2714ac40f56cdd6d
Nan Jiang <njiang028@gmail.com>
2017-01-09 12:15:49 -0500

    chore(addon): Revert the minor version change

--- fabfile.py
+++ fabfile.py
@@ -28,7 +28,7 @@ def _get_dev_version(version):
     """ Get dev version from package.json. It always increments the minor version by 1
     """
     major, minor, patch = version.split('.', 2)
-    return ".".join([major, str(int(minor) + 1), patch])
+    return ".".join([major, minor, str(int(patch) + 1)])
 
 
 def make_dev_manifest(fresh_manifest=True, commit_hash=""):
@@ -45,8 +45,7 @@ def make_dev_manifest(fresh_manifest=True, commit_hash=""):
         manifest["version"] = "{}-dev-{}".format(
             _get_dev_version(manifest["version"]), current_time)
         # Using commit hash in the build string to make it easier to be recognized
-        build_version = "Build: {}-{}".format(
-            _get_dev_version(manifest["version"]), commit_hash)
+        build_version = "Build: {}-{}".format(manifest["version"], commit_hash)
         manifest["description"] = "{}\n\n{}".format(build_version, manifest["description"])
         f.seek(0)
         f.truncate(0)
@@ -68,8 +67,7 @@ def make_prerelease_manifest(fresh_manifest=True, commit_hash=""):
         manifest["version"] = "{}-pre-release-{}".format(
             _get_dev_version(manifest["version"]), current_time)
         # Using commit hash in the build string to make it easier to be recognized
-        build_version = "Build: {}-{}".format(
-            _get_dev_version(manifest["version"]), commit_hash)
+        build_version = "Build: {}-{}".format(manifest["version"], commit_hash)
         manifest["description"] = "{}\n\n{}".format(build_version, manifest["description"])
         f.seek(0)
         f.truncate(0)
