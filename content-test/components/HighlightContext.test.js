fec4682efb7cec55c263ac0cf8f05cf66a79a967
Dan Mosedale <dmose2@gmail.com>
2017-01-11 13:03:49 -0800

    chore(testing: #1929 Port HighlightContext tests to enzyme

--- content-test/components/HighlightContext.test.js
+++ content-test/components/HighlightContext.test.js
@@ -1,73 +1,72 @@
 const React = require("react");
-const ReactDOM = require("react-dom");
-const TestUtils = require("react-addons-test-utils");
 const {shallow} = require("enzyme");
 
+const Tooltip = require("components/Tooltip/Tooltip");
 const {types, PlaceholderHighlightContext, HighlightContext} = require("components/HighlightContext/HighlightContext");
 
 describe("HighlightContext", () => {
-  let instance;
-  let el;
+  let wrapper;
 
   function setup(props = {type: "history"}) {
-    instance = TestUtils.renderIntoDocument(<HighlightContext {...props} />);
-    el = ReactDOM.findDOMNode(instance);
+    wrapper = shallow(<HighlightContext {...props} />);
   }
 
   it("should render the component", () => {
     setup();
-    TestUtils.isCompositeComponentWithType(instance, HighlightContext);
+    assert.instanceOf(wrapper.instance(), HighlightContext);
   });
 
   it("should render the right icon given a type", () => {
     setup({type: "bookmark"});
-    assert.include(instance.refs.icon.className, `icon-${types.bookmark.icon}`);
+    assert.ok(wrapper.find(".icon").hasClass(`icon-${types.bookmark.icon}`));
   });
 
   it("should render props.icon if specified", () => {
     setup({type: "bookmark", icon: "foo"});
-    assert.include(instance.refs.icon.className, "icon-foo");
+    assert.ok(wrapper.find(".icon").hasClass("icon-foo"));
   });
 
   it("should render the label defined by type if props.label is not specified", () => {
     setup({type: "bookmark"});
-    assert.equal(instance.refs.label.textContent, types.bookmark.label);
+    assert.equal(wrapper.find(".hc-label").text(), types.bookmark.label);
   });
 
   it("should render props.label if specified", () => {
     setup({type: "bookmark", label: "My bookmark"});
-    assert.equal(instance.refs.label.textContent, "My bookmark");
+    assert.equal(wrapper.find(".hc-label").text(), "My bookmark");
   });
 
   it("should render props.date if specified", () => {
     setup({type: "bookmark", date: Date.now()});
-    assert.equal(instance.refs.timestamp.textContent, "<1m");
+    assert.equal(wrapper.find(".hc-timestamp").text(), "<1m");
   });
 
   it("should not show a date if props.date is not specified", () => {
     setup({type: "bookmark"});
-    assert.equal(instance.refs.timestamp.textContent, "");
+    assert.lengthOf(wrapper.find(".hc-timestamp").text(), 0);
   });
 
   it("should not show a tooltip if it is not defined by the type", () => {
     setup({type: "bookmark"});
-    assert.isUndefined(instance.refs.tooltip);
+    assert.lengthOf(wrapper.find(Tooltip), 0);
   });
 
   it("should show a tooltip for recommended links", () => {
     setup({type: "recommended", date: Date.now()});
-    assert.ok(instance.refs.tooltip);
-    assert.equal(instance.refs.tooltip.props.label, types.recommended.tooltip);
+    const tooltipWrapper = wrapper.find(Tooltip);
+
+    assert.lengthOf(tooltipWrapper, 1);
+    assert.equal(tooltipWrapper.prop("label"), types.recommended.tooltip);
   });
 
   it("should show not show a timestamps for recommended links", () => {
     setup({type: "recommended", date: Date.now()});
-    assert.equal(instance.refs.timestamp.hidden, true);
+    assert.equal(wrapper.find(".hc-timestamp").prop("hidden"), true);
   });
 
   it("should have a .tooltip-container class", () => {
     setup();
-    assert.include(el.className, "tooltip-container");
+    assert.ok(wrapper.hasClass("tooltip-container"));
   });
 });
 
