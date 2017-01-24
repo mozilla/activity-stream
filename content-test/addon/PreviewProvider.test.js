ac04083c8959a012924ba6dfd9d103d7dd6ff91a
Ursula Sarracini <ursulasarracini@gmail.com>
2017-01-24 15:56:00 -0500

    fix(experiments): Turn MetadataService experiment off

--- content-test/addon/PreviewProvider.test.js
+++ content-test/addon/PreviewProvider.test.js
@@ -3,13 +3,12 @@
 const createPreviewProvider = require("inject!addon/PreviewProvider");
 const {BACKGROUND_FADE} = require("../../common/constants");
 const BACKGROUND_COLOR = [255, 255, 255];
-const mockExperimentProvider = {data: {metadataService: false}};
 const getColor = function() {
   return Promise.resolve(BACKGROUND_COLOR);
 };
 describe("PreviewProvider", () => {
   const {PreviewProvider} = createPreviewProvider({"addon/ColorAnalyzerProvider": {getColor}});
-  const previewProvider = new PreviewProvider({}, {}, mockExperimentProvider);
+  const previewProvider = new PreviewProvider({}, {});
   let fakeSite = {"url": "https://getMyFaviconColor.com", "favicon": "favicon.ico"};
 
   it("should be an get the correct color and set the background fade", () => {
