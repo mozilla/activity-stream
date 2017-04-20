8efea2f643d80dcf393b7d44d5642516335dc666
k88hudson <k88hudson@gmail.com>
2017-04-20 14:44:27 -0400

    style(mc): Style touch-ups from bbell

--- system-addon/content-src/components/TopSites/TopSites.jsx
+++ system-addon/content-src/components/TopSites/TopSites.jsx
@@ -8,12 +8,20 @@ function displayURL(url) {
 const TopSites = props => (<section>
   <h3 className="section-title">Top Sites</h3>
   <ul className="top-sites-list">
-    {props.TopSites.rows.map(link => (<li key={link.url}>
-      <a href={link.url}>
-        <div className="screenshot" style={{backgroundImage: `url(${link.screenshot})`}} />
-        <div className="title">{displayURL(link.url)}</div>
-      </a>
-    </li>))}
+    {props.TopSites.rows.map(link => {
+      const title = displayURL(link.url);
+      const className = `screenshot${link.screenshot ? " active" : ""}`;
+      const style = {backgroundImage: (link.screenshot ? `url(${link.screenshot})` : "none")};
+      return (<li key={link.url}>
+        <a href={link.url}>
+          <div className="tile">
+            <span className="letter-fallback" ariaHidden={true}>{title[0]}</span>
+            <div className={className} style={style} />
+          </div>
+          <div className="title">{title}</div>
+        </a>
+      </li>);
+    })}
   </ul>
 </section>);
 
