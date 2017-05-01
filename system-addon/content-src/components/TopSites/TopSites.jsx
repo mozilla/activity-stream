ccc4d1a6b582f9c68dca827b501cf242b6e324aa
k88hudson <k88hudson@gmail.com>
2017-05-01 17:30:59 -0400

    fix(mc): #2531 Port Top Sites short url code that removes TLD

--- system-addon/content-src/components/TopSites/TopSites.jsx
+++ system-addon/content-src/components/TopSites/TopSites.jsx
@@ -1,15 +1,12 @@
 const React = require("react");
 const {connect} = require("react-redux");
-
-function displayURL(url) {
-  return new URL(url).hostname.replace(/^www./, "");
-}
+const shortURL = require("content-src/lib/short-url");
 
 const TopSites = props => (<section>
   <h3 className="section-title">Top Sites</h3>
   <ul className="top-sites-list">
     {props.TopSites.rows.map(link => {
-      const title = displayURL(link.url);
+      const title = shortURL(link);
       const className = `screenshot${link.screenshot ? " active" : ""}`;
       const style = {backgroundImage: (link.screenshot ? `url(${link.screenshot})` : "none")};
       return (<li key={link.url}>
@@ -26,3 +23,4 @@ const TopSites = props => (<section>
 </section>);
 
 module.exports = connect(state => ({TopSites: state.TopSites}))(TopSites);
+module.exports._unconnected = TopSites;
---
+++ system-addon/content-src/lib/short-url.js
@@ -0,0 +1,26 @@
+/**
+ * shortURL - Creates a short version of a link's url, used for display purposes
+ *            e.g. {url: http://www.foosite.com, eTLD: "com"}  =>  "foosite"
+ *
+ * @param  {obj} link A link object
+ *         {str} link.url (required)- The url of the link
+ *         {str} link.eTLD (required) - The tld of the link
+ *               e.g. for https://foo.org, the tld would be "org"
+ *               Note that this property is added in various queries for ActivityStream
+ *               via Services.eTLD.getPublicSuffix
+ *         {str} link.hostname (optional) - The hostname of the url
+ *               e.g. for http://www.hello.com/foo/bar, the hostname would be "www.hello.com"
+ * @return {str}   A short url
+ */
+module.exports = function shortURL(link) {
+  if (!link.url && !link.hostname) {
+    return "";
+  }
+  const {eTLD} = link;
+  const hostname = (link.hostname || new URL(link.url).hostname).replace(/^www\./i, "");
+
+  // Remove the eTLD (e.g., com, net) and the preceding period from the hostname
+  const eTLDLength = (eTLD || "").length || (hostname.match(/\.com$/) && 3);
+  const eTLDExtra = eTLDLength > 0 ? -(eTLDLength + 1) : Infinity;
+  return hostname.slice(0, eTLDExtra).toLowerCase();
+};
