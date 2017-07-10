4854a908efea91174477fdfb6f4b5df3a47b831b
Andrei Oprea <andrei.br92@gmail.com>
2017-07-10 14:39:41 +0200

    fix(system-addon): Return a topsite label for localhost. Closes #2814.

--- system-addon/test/unit/content-src/lib/short-url.test.js
+++ system-addon/test/unit/content-src/lib/short-url.test.js
@@ -25,4 +25,8 @@ describe("shortURL", () => {
   it("should convert to lowercase", () => {
     assert.equal(shortURL({url: "HTTP://FOO.COM", eTLD: "com"}), "foo");
   });
+
+  it("should return hostname for localhost", () => {
+    assert.equal(shortURL({url: "http://localhost:8000/", eTLD: "localhost"}), "localhost");
+  });
 });
