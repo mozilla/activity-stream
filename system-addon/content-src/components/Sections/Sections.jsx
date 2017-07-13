23e633360627920c3c7edade318af357fc95cdcb
Ursula Sarracini <ursulasarracini@gmail.com>
2017-07-13 13:47:37 -0400

    feat(system-addon): Closes #2818 Port Highlight card UI to system addon

--- system-addon/content-src/components/Sections/Sections.jsx
+++ system-addon/content-src/components/Sections/Sections.jsx
@@ -1,16 +1,16 @@
 const React = require("react");
 const {connect} = require("react-redux");
 const {FormattedMessage} = require("react-intl");
+const Card = require("content-src/components/Card/Card");
 
 class Section extends React.Component {
   render() {
-    const {title, initialized, rows, infoOption} = this.props;
+    const {title, icon, initialized, rows, infoOption, dispatch} = this.props;
     // <Section> <-- React component
     // <section> <-- HTML5 element
-    // Dummy component, finished card component needs to be substituted in here
     return (<section>
         <div className="section-top-bar">
-          <h3 className="section-title"><FormattedMessage {...title} /></h3>
+          <h3 className="section-title"><span className={`icon icon-small-spacer icon-${icon}`} /><FormattedMessage {...title} /></h3>
           {infoOption && <span className="section-info-option">
             <span className="sr-only"><FormattedMessage id="section_info_option" /></span>
             <img className="info-option-icon" />
@@ -31,7 +31,7 @@ class Section extends React.Component {
           </span>}
         </div>
         {initialized && (<ul className="section-list" style={{padding: 0}}>
-          {rows.map(url => url && <img style={{maxHeight: "10em"}} src={url} />)}
+          {rows.map((link, index) => link && <Card index={index} dispatch={dispatch} link={link} />)}
         </ul>)}
       </section>);
   }
@@ -42,7 +42,7 @@ class Sections extends React.Component {
     const sections = this.props.Sections;
     return (
       <div className="sections-list">
-        {sections.map(section => <Section key={section.id} {...section} />)}
+        {sections.map(section => <Section key={section.id} {...section} dispatch={this.props.dispatch} />)}
       </div>
     );
   }
