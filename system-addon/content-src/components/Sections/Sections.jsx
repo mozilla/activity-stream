2b33af18cbecfdaf7b1154a4b3735f071b2ef378
Adam Hillier <a.hillier@outlook.com>
2017-07-18 14:27:14 -0400

    feat(system-addon) Implement section info option. Closes #2875. (#2896)

--- system-addon/content-src/components/Sections/Sections.jsx
+++ system-addon/content-src/components/Sections/Sections.jsx
@@ -1,14 +1,35 @@
 const React = require("react");
 const {connect} = require("react-redux");
+const {FormattedMessage} = require("react-intl");
 
 class Section extends React.Component {
   render() {
-    const {title, initialized, rows} = this.props;
+    const {title, initialized, rows, infoOption} = this.props;
     // <Section> <-- React component
     // <section> <-- HTML5 element
     // Dummy component, finished card component needs to be substituted in here
     return (<section>
-        <h3 className="section-title">{title}</h3>
+        <div className="section-top-bar">
+          <h3 className="section-title"><FormattedMessage {...title} /></h3>
+          {infoOption && <span className="section-info-option">
+            <span className="sr-only"><FormattedMessage id="section_info_option" /></span>
+            <img className="info-option-icon" />
+            <div className="info-option">
+              {infoOption.header &&
+                <div className="info-option-header">
+                  <FormattedMessage {...infoOption.header} />
+                </div>}
+              {infoOption.body &&
+                <p className="info-option-body">
+                  <FormattedMessage {...infoOption.body} />
+                </p>}
+              {infoOption.link &&
+                <a href={infoOption.link.href} target="_blank" rel="noopener noreferrer" className="info-option-link">
+                  <FormattedMessage {...infoOption.link} />
+                </a>}
+            </div>
+          </span>}
+        </div>
         {initialized && (<ul className="section-list" style={{padding: 0}}>
           {rows.map(url => url && <img style={{maxHeight: "10em"}} src={url} />)}
         </ul>)}
