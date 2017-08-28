fda57cc569d6e8030427181be663b08e7da952a7
Nan Jiang <najiang@mozilla.com>
2017-08-28 12:36:31 -0400

    fix(testpilot): Self destruct only for Firefox 56 (#3143)

--- addon/main.js
+++ addon/main.js
@@ -7,6 +7,11 @@ const {TabTracker} = require("addon/TabTracker");
 const {ActivityStreams} = require("addon/ActivityStreams");
 const {setTimeout, clearTimeout} = require("sdk/timers");
 const {Cu} = require("chrome");
+const {getAddonByID} = require("sdk/addon/manager");
+const {get, set} = require("sdk/preferences/service");
+
+const self = require("sdk/self");
+const system = require("sdk/system");
 
 Cu.import("resource://gre/modules/ClientID.jsm");
 Cu.import("resource://gre/modules/Task.jsm");
@@ -30,6 +35,16 @@ Object.assign(exports, {
     options.telemetry = false;
 
     Task.spawn(function*() {
+      let addon = yield getAddonByID(self.id);
+      let version = system.version.split(".")[0];
+      if (addon && version === "56") {
+        const pref = "browser.newtabpage.activity-stream.enabled";
+        if (!get(pref)) {
+          set(pref, true);
+        }
+        addon.uninstall();
+        return;
+      }
       const clientID = yield ClientID.getClientID();
       options.clientID = clientID;
       const tabTracker = new TabTracker(options);
