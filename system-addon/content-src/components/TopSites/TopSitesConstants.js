7357c19584dfb474b2733348336a150c98028da3
Ursula Sarracini <ursulasarracini@gmail.com>
2017-09-08 15:54:15 -0400

    feat(topsites): Set Top Site card image source to high-res icon when available

--- system-addon/content-src/components/TopSites/TopSitesConstants.js
+++ system-addon/content-src/components/TopSites/TopSitesConstants.js
@@ -1,5 +1,7 @@
 module.exports = {
   TOP_SITES_SOURCE: "TOP_SITES",
   TOP_SITES_CONTEXT_MENU_OPTIONS: ["CheckPinTopSite", "Separator", "OpenInNewWindow",
-    "OpenInPrivateWindow", "Separator", "BlockUrl", "DeleteUrl"]
+    "OpenInPrivateWindow", "Separator", "BlockUrl", "DeleteUrl"],
+  // minimum size necessary to show a rich icon instead of a screenshot
+  MIN_FAVICON_SIZE: 96
 };
