c41b4e5fbb38820666e5ff2f67f293233f0602ce
Ed Lee <edilee@mozilla.com>
2018-03-22 14:11:40 -0700

    fix(screenshots): Store an empty screenshot to avoid repeatedly requesting failures (#4050)
    Fix Bug 1384094 - High CPU. network traffic and memory usage when open Newtab page if you have opened / bookmarked a problematic/ huge game page in the past

--- system-addon/test/unit/lib/Screenshots.test.js
+++ system-addon/test/unit/lib/Screenshots.test.js
@@ -1,6 +1,6 @@
 "use strict";
+import {EMPTY_SCREENSHOT, Screenshots} from "lib/Screenshots.jsm";
 import {GlobalOverrider} from "test/unit/utils";
-import {Screenshots} from "lib/Screenshots.jsm";
 
 const URL = "foo.com";
 const FAKE_THUMBNAIL_PATH = "fake/path/thumb.jpg";
@@ -24,7 +24,10 @@ describe("Screenshots", () => {
       }
     };
     globals.set("BackgroundPageThumbs", {captureIfMissing: sandbox.spy(() => Promise.resolve())});
-    globals.set("PageThumbs", {getThumbnailPath: sandbox.spy(() => Promise.resolve(FAKE_THUMBNAIL_PATH))});
+    globals.set("PageThumbs", {
+      _store: sandbox.stub(),
+      getThumbnailPath: sandbox.spy(() => Promise.resolve(FAKE_THUMBNAIL_PATH))
+    });
     globals.set("PrivateBrowsingUtils", {isWindowPrivate: sandbox.spy(() => false)});
     globals.set("OS", {File: {open: sandbox.spy(() => Promise.resolve({read: () => [], close: () => {}}))}});
     globals.set("FileUtils", {File: sandbox.spy(() => {})});
@@ -56,10 +59,13 @@ describe("Screenshots", () => {
       await Screenshots.getScreenshotForURL(URL);
       assert.calledOnce(global.MIMEService.getTypeFromFile);
     });
-    it("should throw if something goes wrong", async () => {
+    it("should get empty if something goes wrong", async () => {
       globals.set("BackgroundPageThumbs", {captureIfMissing: () => new Error("Cannot capture tumbnail")});
+
       const screenshot = await Screenshots.getScreenshotForURL(URL);
-      assert.equal(screenshot, null);
+
+      assert.calledOnce(global.PageThumbs._store);
+      assert.equal(screenshot, EMPTY_SCREENSHOT);
     });
   });
 
