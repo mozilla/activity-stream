7da985561ffdd32a5ee8823b4cf390b0b6de5c0a
k88hudson <k88hudson@gmail.com>
2018-04-21 12:54:50 -0400

    style(as-router): AS router templates should respect theme variables

--- system-addon/content-src/message-center/components/Button.jsx
+++ system-addon/content-src/message-center/components/Button.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const styles = {
   button: {
-    border: 0,
-    backgroundColor: "#e1e1e2",
+    whiteSpace: "nowrap",
+    borderRadius: "4px",
+    border: "1px solid var(--newtab-border-secondary-color)",
+    backgroundColor: "var(--newtab-button-secondary-color)",
     fontFamily: "inherit",
     padding: "8px 15px",
-    marginLeft: "15px"
+    marginLeft: "12px",
+    color: "inherit"
   }
 };
 
