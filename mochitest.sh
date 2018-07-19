3cd13efd0d3c6b2acf2b7b6d82166c8427d501cb
Andrei Oprea <andrei.br92@gmail.com>
2018-07-19 19:06:38 +0200

    Add additional tests to run

--- mochitest.sh
+++ mochitest.sh
@@ -6,10 +6,15 @@ export SHELL=/bin/bash
 cd /mozilla-central && hg pull && hg update -C
 
 # Build Activity Stream and copy the output to m-c
-cd /activity-stream && npm run buildmc
+cd /activity-stream && npm install . && npm run buildmc
 
 # Build latest m-c with Activity Stream changes
-cd /mozilla-central && ./mach build  
-
-# Run the mochitests
-cd /activity-stream && npm run mochitest
+cd /mozilla-central && ./mach build \
+  && ./mach lint -l eslint -l codespell browser/extensions/activity-stream \
+  && ./mach test browser/extensions/activity-stream --headless \
+  && ./mach test browser/components/newtab/tests/browser --headless \
+  && ./mach test browser/components/newtab/tests/xpcshell \
+  && ./mach test browser/components/preferences/in-content/tests/browser_hometab_restore_defaults.js --headless \
+  && ./mach test browser/components/preferences/in-content/tests/browser_newtab_menu.js --headless \
+  && ./mach test browser/components/enterprisepolicies/tests/browser/browser_policy_set_homepage.js --headless \
+  && ./mach test browser/components/preferences/in-content/tests/browser_search_subdialogs_within_preferences_1.js --headless
