8a20ba8d34a3c77054ed9c378a2f8e5041ed4deb
Andrei Oprea <andrei.br92@gmail.com>
2018-07-23 12:46:45 +0200

    Run browser_parsable_css tests

--- mochitest.sh
+++ mochitest.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export SHELL=/bin/bash
+# Display required for `browser_parsable_css` tests
+export DISPLAY=:99.0
+/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16 -extension RANDR
 
 # Pull latest m-c and update tip
 cd /mozilla-central && hg pull && hg update -C
@@ -10,6 +13,7 @@ cd /activity-stream && npm install . && npm run buildmc
 
 # Build latest m-c with Activity Stream changes
 cd /mozilla-central && ./mach build \
+  && ./mach test browser_parsable_css \
   && ./mach lint -l eslint -l codespell browser/extensions/activity-stream \
   && ./mach test browser/extensions/activity-stream --headless \
   && ./mach test browser/components/newtab/tests/browser --headless \
