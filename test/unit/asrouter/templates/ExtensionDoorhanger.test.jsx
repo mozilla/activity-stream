460baf76c8168c6e646f7267a7b1b798231867a6
Andrei Oprea <andrei.br92@gmail.com>
2018-08-22 16:53:14 +0200

    Include sumo tooltip in schema

--- test/unit/asrouter/templates/ExtensionDoorhanger.test.jsx
+++ test/unit/asrouter/templates/ExtensionDoorhanger.test.jsx
@@ -1,7 +1,10 @@
 import schema from "content-src/asrouter/templates/CFR/templates/ExtensionDoorhanger.schema.json";
 
 const DEFAULT_CONTENT = {
-  "heading": "Recommended Extension",
+  "heading": {
+    "title": "Recommended Extension",
+    "url": "https://support.mozilla.org"
+  },
   "addon": {
     "title": "Addon name",
     "icon": "base64",
@@ -16,7 +19,7 @@ const DEFAULT_CONTENT = {
   }
 };
 
-describe.only("ExtensionDoorhanger", () => {
+describe("ExtensionDoorhanger", () => {
   it("should validate DEFAULT_CONTENT", () => {
     assert.jsonSchema(DEFAULT_CONTENT, schema);
   });
