141f3cfad5a39c37461eefdcffa32830b2477791
Andrei Oprea <andrei.br92@gmail.com>
2018-08-23 17:02:44 +0200

    Update schema

--- test/unit/asrouter/templates/ExtensionDoorhanger.test.jsx
+++ test/unit/asrouter/templates/ExtensionDoorhanger.test.jsx
@@ -1,8 +1,10 @@
 import schema from "content-src/asrouter/templates/CFR/templates/ExtensionDoorhanger.schema.json";
 
 const DEFAULT_CONTENT = {
-  "heading": {
-    "title": "Recommended Extension",
+  "notification_text": "Recommendation",
+  "heading_text": "Recommended Extension",
+  "info_icon": {
+    "label": "why_seeing_this",
     "url": "https://support.mozilla.org"
   },
   "addon": {
@@ -10,14 +12,22 @@ const DEFAULT_CONTENT = {
     "icon": "base64",
     "author": "Author name"
   },
-  "content": {
-    "text": "Description of addon",
-    "url": "https://example.com"
+  "text": "Description of addon",
+  "amo_url": "https://example.com",
+  "buttons": {
+    "primary": {
+      "label": "btn_ok",
+      "action": {
+        "type": "INSTALL_ADDON_FROM_URL",
+        "data": {"url": "https://example.com"}
+      }
+    },
+    "secondary": {"label": "btn_cancel"}
   }
 };
 
 describe("ExtensionDoorhanger", () => {
-  it("should validate DEFAULT_CONTENT", () => {
+  it.only("should validate DEFAULT_CONTENT", () => {
     assert.jsonSchema(DEFAULT_CONTENT, schema);
   });
 });
