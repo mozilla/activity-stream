f0005f64b9977d69ae225df3249688d8860c287c
Ursula Sarracini <ursulasarracini@gmail.com>
2018-10-22 14:25:05 -0400

    nobug(chore): Create a buildlibrary task for about:library

---
+++ content-src/aboutlibrary/aboutlibrary.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class LibraryRouter extends React.PureComponent {
+  render() {
+    return (<div className="under-construction" />);
+  }
+}
+ReactDOM.render(<LibraryRouter />, document.body);
