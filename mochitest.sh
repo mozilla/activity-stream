b9bb0a511f80df68e194f3fc71b58eb6af10f3fb
Andrei Oprea <andrei.br92@gmail.com>
2019-01-04 12:13:26 +0200

    Fix Bug 1517342 - Taskcluster fails because of missing env var

--- mochitest.sh
+++ mochitest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export SHELL=/bin/bash
+export TASKCLUSTER_ROOT_URL="https://taskcluster.net"
 # Display required for `browser_parsable_css` tests
 export DISPLAY=:99.0
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16 -extension RANDR
