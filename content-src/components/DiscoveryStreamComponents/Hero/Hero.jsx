5b51cb88c52fea2290e48252254c9d5e8c3754be
Gavin Lazar Suntop <gavin@gsuntop.com>
2019-01-07 12:55:09 -0800

    wip

--- content-src/components/DiscoveryStreamComponents/Hero/Hero.jsx
+++ content-src/components/DiscoveryStreamComponents/Hero/Hero.jsx
@@ -3,8 +3,15 @@ import React from "react";
 export class Hero extends React.PureComponent {
   render() {
     return (
-      <div>
-        Hero
+      <div className="ds-hero">
+        <div className="wrapper">
+          <img src="https://placekitten.com/576/324"/>
+          <div className="meta">
+            <header>Lorem Ipsum</header>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quod, adipisci culpa ad ex officia totam quas animi non esse in quaerat consectetur sint at veritatis! Voluptatibus incidunt quidem facere!</p>
+            <p>Source</p>
+          </div>
+        </div>
       </div>
     );
   }
