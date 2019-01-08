6e7678a144b00d1b17bfdffd282a68b59626e3ce
Gavin Lazar Suntop <gavin@gsuntop.com>
2019-01-08 10:46:10 -0800

    lint

--- content-src/components/DiscoveryStreamComponents/Hero/Hero.jsx
+++ content-src/components/DiscoveryStreamComponents/Hero/Hero.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
 export class Hero extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
-
   // TODO: Un-hardcode all these values
   render() {
     return (
