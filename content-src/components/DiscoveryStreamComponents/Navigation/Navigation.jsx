e268b12c06536b30655afba1185242c22e448135
Scott <scott.downe@gmail.com>
2019-01-22 13:51:56 -0500

    Remove arrow from css and html

--- content-src/components/DiscoveryStreamComponents/Navigation/Navigation.jsx
+++ content-src/components/DiscoveryStreamComponents/Navigation/Navigation.jsx
@@ -11,12 +11,10 @@ export class Navigation extends React.PureComponent {
   render() {
     const {links} = this.props || [];
     const {alignment} = this.props || "centered";
-    const moreRecs = links.pop();
     return (
       <span className={`ds-navigation ds-navigation-${alignment}`}>
         <ul>
           {links && links.map(t => <Topic key={t.name} url={t.url} name={t.name} />)}
-          {moreRecs ? <li><a href={moreRecs.url} className="ds-more-recommendations">{moreRecs.name}</a></li> : null }
         </ul>
       </span>
     );
