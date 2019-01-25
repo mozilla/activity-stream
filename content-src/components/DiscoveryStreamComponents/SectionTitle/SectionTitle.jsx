96b874fc56784238a852bf80e5cdb355d13d3733
Andrei Oprea <andrei.br92@gmail.com>
2019-01-25 18:02:22 +0100

    Revert "Ensure we don't use object destructuring for server sent values"
    This reverts commit 212331659a2111be019eba90fe21cefab40e4e4f.

--- content-src/components/DiscoveryStreamComponents/SectionTitle/SectionTitle.jsx
+++ content-src/components/DiscoveryStreamComponents/SectionTitle/SectionTitle.jsx
@@ -2,16 +2,11 @@ import React from "react";
 
 export class SectionTitle extends React.PureComponent {
   render() {
-    const {header} = this.props;
-
-    if (!header) {
-      return null;
-    }
-
+    const {header: {title, subtitle}} = this.props;
     return (
       <div className="ds-section-title">
-        <div className="title">{header.title}</div>
-        {header.subtitle ? <div className="subtitle">{header.subtitle}</div> : null}
+        <div className="title">{title}</div>
+        {subtitle ? <div className="subtitle">{subtitle}</div> : null}
       </div>
     );
   }
