55ef6c13a1bdeb5d857390b5475510840cb1890c
Ed Lee <edilee@mozilla.com>
2019-01-25 06:23:47 -0800

    Bug 1519878 - implement full width ds-list items (#4726)

--- content-src/components/DiscoveryStreamComponents/List/List.jsx
+++ content-src/components/DiscoveryStreamComponents/List/List.jsx
@@ -1,6 +1,7 @@
 import {actionCreators as ac} from "common/Actions.jsm";
 import {connect} from "react-redux";
 import React from "react";
+import {truncateText} from "content-src/lib/truncate-text";
 
 /**
  * @note exported for testing only
@@ -39,6 +40,7 @@ export class ListItem extends React.PureComponent {
                 {this.props.title}
               </b>
             </div>
+            {this.props.excerpt && <div className="ds-list-item-excerpt">{truncateText(this.props.excerpt, 90)}</div>}
             <div className="ds-list-item-info">{this.props.domain}</div>
           </div>
           <div className="ds-list-image" style={{backgroundImage: `url(${this.props.image_src})`}} />
@@ -66,6 +68,7 @@ export function _List(props) {
 
   const listStyles = [
     "ds-list",
+    props.fullWidth ? "ds-list-full-width" : "",
     props.hasBorders ? "ds-list-borders" : "",
     props.hasImages ? "ds-list-images" : "",
     props.hasNumbers ? "ds-list-numbers" : "",
@@ -80,6 +83,7 @@ export function _List(props) {
 
 _List.defaultProps = {
   recStartingPoint: 0, // Index of recommendations to start displaying from
+  fullWidth: false, // Display items taking up the whole column
   hasBorders: false, // Display lines separating each item
   hasImages: false, // Display images for each item
   hasNumbers: false, // Display numbers for each item
