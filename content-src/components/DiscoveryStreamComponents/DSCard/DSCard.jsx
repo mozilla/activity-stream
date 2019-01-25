dc5ee12233dd948870938a240f4645e39ce6faca
Scott <scott.downe@gmail.com>
2019-01-25 12:17:08 -0500

    Bug 1522680 - Source for spocs

--- content-src/components/DiscoveryStreamComponents/DSCard/DSCard.jsx
+++ content-src/components/DiscoveryStreamComponents/DSCard/DSCard.jsx
@@ -32,11 +32,8 @@ export class DSCard extends React.PureComponent {
         <div className="meta">
           <header className="title">{this.props.title}</header>
           {this.props.excerpt && <p className="excerpt">{this.props.excerpt}</p>}
-          {this.props.context ? (
-            <p className="context">{this.props.context}</p>
-          ) : (
-            <p className="source">{this.props.source}</p>
-          )}
+          {this.props.context && <p className="context">{this.props.context}</p>}
+          <p className="source">{this.props.source}</p>
         </div>
       </a>
     );
