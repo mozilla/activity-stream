d03a962c5951495166bc54af97e58854a4f25f2f
Gavin Lazar Suntop <gavin@gsuntop.com>
2019-02-01 11:49:21 -0800

    adding icon

--- content-src/components/DiscoveryStreamComponents/DSMessage/DSMessage.jsx
+++ content-src/components/DiscoveryStreamComponents/DSMessage/DSMessage.jsx
@@ -4,8 +4,13 @@ export class DSMessage extends React.PureComponent {
   render() {
     return (
       <div className="ds-message">
-        {this.props.title && (<header className="title">{this.props.title}</header>)}
-        {this.props.subtitle && (
+        {this.props.title && (
+          <header className="title">
+            {this.props.icon && (<img src={this.props.icon}/>)}
+            <span>{this.props.title}</span>
+          </header>
+        )}
+        {(this.props.subtitle || this.props.link_text && this.props.link_url) && (
           <p className="subtitle">
             {this.props.subtitle && (<span>{this.props.subtitle}</span>)}
             {this.props.link_text && this.props.link_url && (<a href={this.props.link_url}>{this.props.link_text}</a>)}
