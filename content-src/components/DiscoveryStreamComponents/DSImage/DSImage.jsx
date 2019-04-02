2cdf1f0fbe8711f487b4142750a6ed78ddbdea8f
Gavin Lazar Suntop <gavin@gsuntop.com>
2019-04-02 16:40:51 -0700

    further utilize thumbor

--- content-src/components/DiscoveryStreamComponents/DSImage/DSImage.jsx
+++ content-src/components/DiscoveryStreamComponents/DSImage/DSImage.jsx
@@ -7,21 +7,17 @@ export class DSImage extends React.PureComponent {
   }
 
   // Change the image URL to request a size tailored for the parent container width
+  // Also: force JPEG, quality 60, no upscaling, no EXIF data
+  // Uses Thumbor: https://thumbor.readthedocs.io/en/latest/usage.html
   reformatImageURL(url, width) {
-    // Determine if URL matches Thumbor spec
-    // https://thumbor.readthedocs.io/en/latest/usage.html
-    if (url && width && url.match(/&resize=[^&]*/)) {
-      url = url.replace(/&resize=[^&]*$/, `&resize=w${width}`);
-    }
-
-    return url;
+    return `https://pocket-image-cache.com/${width}x0/filters:format(jpeg):quality(60):no_upscale():strip_exif()/${encodeURIComponent(url)}`;
   }
 
   componentDidMount() {
     let parentMeasurement = ReactDOM.findDOMNode(this).parentNode.clientWidth;
 
-    // Quirk: Whenever the `Network` tab is open in devtools this is sometimes too large
-    // This will keep it from ever being larger than the overall container width in edge cases
+    // Quirk: Whenever the `Network` tab is open in devtools this is sometimes too large (eg: resolves to window width)
+    // This will keep it from ever being larger than the overall *container width* in these edge cases
     parentMeasurement = parentMeasurement > 936 ? 936 : parentMeasurement;
 
     this.setState({
@@ -46,7 +42,7 @@ export class DSImage extends React.PureComponent {
        );
     }
 
-    let img = null;
+    let img;
 
     if (source && source2x) {
       img = (<img src={source} srcset={`${source2x} 2x`} />);
@@ -70,7 +66,8 @@ TODO:
 + detect retina and use 1x if not present
 + memoize images so that smaller versions aren't fetched unnecissarily
 + 2 caches for 1x and 2x (because one set will not get loaded)
-- force jpeg (need cleaner img urls - in progress)
++ force jpeg (need cleaner img urls - in progress)
+- force aspect ratio?
 - enable lazy loading (probably follow-on ticket)
 
 */
