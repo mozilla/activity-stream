8c999f7706c0e3552e6ba493100a3554795d5aba
Andrei Oprea <andrei.br92@gmail.com>
2019-04-30 19:31:23 +0200

    typo

--- test/browser/browser_asrouter_targeting.js
+++ test/browser/browser_asrouter_targeting.js
@@ -162,7 +162,7 @@ add_task(async function check_isFxAEnabled() {
 add_task(async function check_isFxAEnabled() {
   await pushPrefs(["identity.fxaccounts.enabled", true]);
   is(await ASRouterTargeting.Environment.isFxAEnabled, true,
-    "should return true if fxa is disabled");
+    "should return true if fxa is enabled");
 
   const message = {id: "foo", targeting: "isFxAEnabled"};
   is(await ASRouterTargeting.findMatchingMessage({messages: [message]}), message,
