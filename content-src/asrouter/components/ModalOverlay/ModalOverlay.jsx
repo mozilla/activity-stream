d097c8d07119ac9c87323057aedbf0312fdf2710
k88hudson <k88hudson@gmail.com>
2019-05-01 13:37:53 -0400

    Bug 1548267 - Reformat without hooks

--- content-src/asrouter/components/ModalOverlay/ModalOverlay.jsx
+++ content-src/asrouter/components/ModalOverlay/ModalOverlay.jsx
@@ -1,53 +1,45 @@
 import React from "react";
-const {useEffect} = React;
 
-export const useEscapeKeyCallback = props => {
-  const {isActive, callback} = props;
-  const doc = props.document || global.document;
+export class ModalOverlayWrapper extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.onKeyDown = this.onKeyDown.bind(this);
+  }
 
-  const onKeyDown = event => {
-    if (event.key === "Escape") { callback(); }
-  };
-
-  useEffect(() => {
-    if (isActive) {
-      doc.addEventListener("keydown", onKeyDown);
-      return () => {
-        doc.removeEventListener("keydown", onKeyDown);
-      };
+  onKeyDown(event) {
+    if (event.key === "Escape") {
+      this.props.onClose();
     }
-    return undefined;
-  });
-};
+  }
 
-export const ModalOverlayWrapper = props => {
-  // If you press escape, close the modal!
-  useEscapeKeyCallback({isActive: props.active, callback: props.onClose, document: props.document});
-
-  return (<React.Fragment>
-    <div className={`modalOverlayOuter ${props.active ? "active" : ""}`} onClick={props.onClose} />
-    <div className={`modalOverlayInner ${props.active ? "active" : ""} ${props.innerClassName || ""}`}>
-      {props.children}
-    </div>
-  </React.Fragment>);
-};
-
-export class ModalOverlay extends React.PureComponent {
   componentWillMount() {
-    this.setState({active: true});
-    document.body.classList.add("modal-open");
+    this.props.document.addEventListener("keydown", this.onKeyDown);
+    this.props.document.body.classList.add("modal-open");
   }
 
   componentWillUnmount() {
-    document.body.classList.remove("modal-open");
-    this.setState({active: false});
+    this.props.document.removeEventListener("keydown", this.onKeyDown);
+    this.props.document.body.classList.remove("modal-open");
   }
 
   render() {
-    const {active} = this.state;
+    const {props} = this;
+    return (<React.Fragment>
+      <div className="modalOverlayOuter active" onClick={props.onClose} role="presentation" />
+      <div className={`modalOverlayInner active ${props.innerClassName || ""}`}>
+        {props.children}
+      </div>
+    </React.Fragment>);
+  }
+}
+
+ModalOverlayWrapper.defaultProps = {document: global.document};
+
+export class ModalOverlay extends React.PureComponent {
+  render() {
     const {title, button_label} = this.props;
     return (
-      <ModalOverlayWrapper active={active}>
+      <ModalOverlayWrapper onClose={this.props.onDoneButton}>
         <h2> {title} </h2>
         {this.props.children}
         <div className="footer">
--- test/unit/asrouter/ModalOverlay.test.jsx
+++ test/unit/asrouter/ModalOverlay.test.jsx
@@ -7,15 +7,48 @@ describe("ModalOverlayWrapper", () => {
   let sandbox;
   beforeEach(() => {
     sandbox = sinon.createSandbox();
-    fakeDoc = {addEventListener: sandbox.stub(), removeEventListener: sandbox.stub()};
+    fakeDoc = {
+      addEventListener: sandbox.stub(),
+      removeEventListener: sandbox.stub(),
+      body: {classList: {add: sandbox.stub(), remove: sandbox.stub()}},
+    };
   });
   afterEach(() => {
     sandbox.restore();
   });
-  it("should do stuff", async () => {
-    sandbox.stub(global, "addEventListener");
-    const wrapper = mount(<ModalOverlayWrapper active={true} document={fakeDoc} />);
-    await new Promise(resolve => setTimeout(resolve, 0));
+  it("should add eventListener and a class on mount", async () => {
+    mount(<ModalOverlayWrapper document={fakeDoc} />);
     assert.calledOnce(fakeDoc.addEventListener);
+    assert.calledWith(fakeDoc.body.classList.add, "modal-open");
+  });
+
+  it("should remove eventListener on unmount", async () => {
+    const wrapper = mount(<ModalOverlayWrapper document={fakeDoc} />);
+    wrapper.unmount();
+    assert.calledOnce(fakeDoc.addEventListener);
+    assert.calledOnce(fakeDoc.removeEventListener);
+    assert.calledWith(fakeDoc.body.classList.remove, "modal-open");
+  });
+
+  it("should call props.onClose on an Escape key", async () => {
+    const onClose = sandbox.stub();
+    mount(<ModalOverlayWrapper document={fakeDoc} onClose={onClose} />);
+
+    // Simulate onkeydown being called
+    const [, callback] = fakeDoc.addEventListener.firstCall.args;
+    callback({key: "Escape"});
+
+    assert.calledOnce(onClose);
+  });
+
+  it("should not call props.onClose on other keys than Escape", async () => {
+    const onClose = sandbox.stub();
+    mount(<ModalOverlayWrapper document={fakeDoc} onClose={onClose} />);
+
+    // Simulate onkeydown being called
+    const [, callback] = fakeDoc.addEventListener.firstCall.args;
+    callback({key: "Ctrl"});
+
+    assert.notCalled(onClose);
   });
 });
