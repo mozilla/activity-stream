4fe295e0e5738c4de6de80564374789a60b95bbd
k88hudson <k88hudson@gmail.com>
2019-05-23 15:25:00 -0400

    Bug 1553917 - Make eslint compatible with mozilla central

--- .eslintrc.jsx-a11y.js
+++ .eslintrc.jsx-a11y.js
@@ -3,15 +3,28 @@ module.exports = {
     "jsx-a11y" // require("eslint-plugin-jsx-a11y")
   ],
   "extends": "plugin:jsx-a11y/recommended",
-  "overrides": [{
-    // These files use fluent-dom to insert content
-    "files": [
-      "content-src/asrouter/templates/OnboardingMessage/**",
-      "content-src/asrouter/templates/Trailhead/**",
-    ],
-    "rules": {
-      "jsx-a11y/anchor-has-content": 0,
-      "jsx-a11y/heading-has-content": 0,
+  "overrides": [
+    {
+      // These files use fluent-dom to insert content
+      "files": [
+        "content-src/asrouter/templates/OnboardingMessage/**",
+        "content-src/asrouter/templates/Trailhead/**",
+      ],
+      "rules": {
+        "jsx-a11y/anchor-has-content": 0,
+        "jsx-a11y/heading-has-content": 0,
+      }
+    },
+    {
+      // These files use fluent-dom to insert content
+      "files": [
+        "content-src/asrouter/templates/OnboardingMessage/**",
+        "content-src/asrouter/templates/Trailhead/**",
+      ],
+      "rules": {
+        "jsx-a11y/anchor-has-content": 0,
+        "jsx-a11y/heading-has-content": 0,
+      }
     }
-  }],
+  ],
 };
