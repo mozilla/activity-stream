8276469481472a1155a212419e59b00f14e1ee42
Ed Lee <edilee@mozilla.com>
2019-05-30 11:58:07 -0700

    Bug 1555781 - Convert Search component to use fluent

--- bin/bug_xxx_newtab.py
+++ bin/bug_xxx_newtab.py
@@ -57,5 +57,14 @@ def migrate(ctx):
 
 newtab-page-title = { COPY(from_path, "newtab_page_title") }
 
+newtab-search-box-search-text = { COPY(from_path, "search_button") }
+newtab-search-box-search-button =
+    .title = { COPY(from_path, "search_button") }
+
+newtab-search-box-search-the-web-text = { COPY(from_path, "search_web_placeholder") }
+newtab-search-box-search-the-web-input =
+    .placeholder = { COPY(from_path, "search_web_placeholder") }
+    .title = { COPY(from_path, "search_web_placeholder") }
+
         """, from_path='browser/chrome/browser/activity-stream/newtab.properties')
     )
