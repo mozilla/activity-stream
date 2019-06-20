87be373122148e49fd949ec6ef8f291cb18a20be
Jeane Carlos <volbohel@gmail.com>
2019-06-20 15:29:16 -0700

    Bug 1557874 - Convert link menu strings in New Tab to use fluent (#5104)
    * Bug 1557874 - Convert link menu strings in New Tab to use fluent
    
    * Bug 1557874 - Updating fluent strings for pocket cards
    
    * Bug 1557874 - Context Menu platform strings migrated to fluent
    
    * Bug 1557874 - Pocket brand name added to fluent strings
    
    * Bug 1557874 - Fluent changes related to the section menu and area.
    
    * Reverting some changes to resolve screen reader changes made in PR  #5099
    
    * Bug 1557874 - Updated SectionMenu and LinkMenu test cases
    
    * Minor fixes to clean PR
    
    * Bug 1557874 - Fixed mochi tests and .py linting errors
    
    * Parsing fix....
    
    * Resolving feedback related to condensing data-l10n-id usage
    
    * Fixed failed test case reading intlID instead of fluentID

--- bin/bug_1485002_newtab.py
+++ bin/bug_1485002_newtab.py
@@ -4,10 +4,10 @@
 # http://creativecommons.org/publicdomain/zero/1.0/
 
 from __future__ import absolute_import
-# import fluent.syntax.ast as FTL
+import fluent.syntax.ast as FTL
 from fluent.migrate.helpers import transforms_from
-# from fluent.migrate.helpers import MESSAGE_REFERENCE, TERM_REFERENCE
-# from fluent.migrate import COPY, CONCAT, REPLACE
+from fluent.migrate.helpers import TERM_REFERENCE, VARIABLE_REFERENCE
+from fluent.migrate import REPLACE
 
 TARGET_FILE = 'browser/browser/newtab/newtab.ftl'
 SOURCE_FILE = TARGET_FILE
@@ -83,13 +83,13 @@ newtab-topsites-url-label = { COPY(from_path, "topsites_form_url_label") }
 newtab-topsites-url-input =
     .placeholder = { COPY(from_path, "topsites_form_url_placeholder") }
 newtab-topsites-url-validation = { COPY(from_path, "topsites_form_url_validation") }
-newtab-topsites-delete-history-button = { COPY(from_path, "menu_action_delete") }
 
 newtab-topsites-image-url-label = { COPY(from_path, "topsites_form_image_url_label") }
 newtab-topsites-use-image-link = { COPY(from_path, "topsites_form_use_image_link") }
 newtab-topsites-image-validation = { COPY(from_path, "topsites_form_image_validation") }
 
 newtab-topsites-cancel-button = { COPY(from_path, "topsites_form_cancel_button") }
+newtab-topsites-delete-history-button = { COPY(from_path, "menu_action_delete") }
 newtab-topsites-save-button = { COPY(from_path, "topsites_form_save_button") }
 newtab-topsites-preview-button = { COPY(from_path, "topsites_form_preview_button") }
 newtab-topsites-add-button = { COPY(from_path, "topsites_form_add_button") }
@@ -98,6 +98,110 @@ newtab-confirm-delete-history-p1 = { COPY(from_path, "confirm_history_delete_p1"
 newtab-confirm-delete-history-p2 = { COPY(from_path, "confirm_history_delete_notice_p2") }
 
 newtab-topsites-add-search-engine = { COPY(from_path, "section_menu_action_add_search_engine") }
+newtab-topsites-tooltip =
+    .title = { COPY(from_path, "context_menu_title") }
+newtab-topsites-placeholder-tooltip =
+    .title = { COPY(from_path, "edit_topsites_edit_button") }
+
+newtab-menu-edit-topsites = { COPY(from_path, "edit_topsites_button_text") }
+newtab-menu-open-new-window = { COPY(from_path, "menu_action_open_new_window") }
+newtab-menu-open-new-private-window = { COPY(from_path, "menu_action_open_private_window") }
+newtab-menu-dismiss = { COPY(from_path, "menu_action_dismiss") }
+newtab-menu-pin = { COPY(from_path, "menu_action_pin") }
+newtab-menu-unpin = { COPY(from_path, "menu_action_unpin") }
+newtab-menu-delete-history = { COPY(from_path, "menu_action_delete") }
+newtab-menu-remove-bookmark = { COPY(from_path, "menu_action_remove_bookmark") }
+newtab-menu-bookmark = { COPY(from_path, "menu_action_bookmark") }
+
+newtab-menu-copy-download-link = { COPY(from_path, "menu_action_copy_download_link") }
+newtab-menu-go-to-download-page = { COPY(from_path, "menu_action_go_to_download_page") }
+newtab-menu-remove-download = { COPY(from_path, "menu_action_remove_download") }
+
+
+newtab-menu-show-file =
+    { PLATFORM() ->
+      [macos] { COPY(from_path, "menu_action_show_file_mac_os") }
+      [windows] { COPY(from_path, "menu_action_show_file_windows") }
+      [linux] { COPY(from_path, "menu_action_show_file_linux") }
+       *[other] { COPY(from_path, "menu_action_show_file_default") }
+    }
+newtab-menu-open-file = { COPY(from_path, "menu_action_open_file") }
+
+newtab-card-tooltip =
+    .title = { COPY(from_path, "context_menu_title") }
+newtab-label-visited = { COPY(from_path, "type_label_visited") }
+newtab-label-bookmarked = { COPY(from_path, "type_label_bookmarked") }
+newtab-label-recommended = { COPY(from_path, "type_label_recommended") }
+newtab-label-download = { COPY(from_path, "type_label_downloaded") }
+
+newtab-section-menu-remove-section = { COPY(from_path, "section_menu_action_remove_section") }
+newtab-section-menu-collapse-section = { COPY(from_path, "section_menu_action_collapse_section") }
+newtab-section-menu-expand-section = { COPY(from_path, "section_menu_action_expand_section") }
+newtab-section-menu-manage-section = { COPY(from_path, "section_menu_action_manage_section") }
+newtab-section-menu-manage-webext = { COPY(from_path, "section_menu_action_manage_webext") }
+newtab-section-menu-add-topsite = { COPY(from_path, "section_menu_action_add_topsite") }
+newtab-section-menu-add-search-engine =
+    { COPY(from_path, "section_menu_action_add_search_engine") }
+newtab-section-menu-move-up = { COPY(from_path, "section_menu_action_move_up") }
+newtab-section-menu-move-down = { COPY(from_path, "section_menu_action_move_down") }
+newtab-section-menu-privacy-notice = { COPY(from_path, "section_menu_action_privacy_notice") }
+newtab-empty-section-highlights = { COPY(from_path, "highlights_empty_state") }
 
         """, from_path='browser/chrome/browser/activity-stream/newtab.properties')
+    ),
+    ctx.add_transforms(
+        TARGET_FILE,
+        SOURCE_FILE,
+        [
+            FTL.Message(
+                id=FTL.Identifier("newtab-menu-save-to-pocket"),
+                value=REPLACE(
+                    "browser/chrome/browser/activity-stream/newtab.properties",
+                    "menu_action_save_to_pocket",
+                    {
+                        "Pocket": TERM_REFERENCE("pocket-brand-name")
+                    },
+                )
+            ),
+            FTL.Message(
+                id=FTL.Identifier("newtab-menu-archive-pocket"),
+                value=REPLACE(
+                    "browser/chrome/browser/activity-stream/newtab.properties",
+                    "menu_action_archive_pocket",
+                    {
+                        "Pocket": TERM_REFERENCE("pocket-brand-name")
+                    },
+                )
+            ),
+            FTL.Message(
+                id=FTL.Identifier("newtab-menu-delete-pocket"),
+                value=REPLACE(
+                    "browser/chrome/browser/activity-stream/newtab.properties",
+                    "menu_action_delete_pocket",
+                    {
+                        "Pocket": TERM_REFERENCE("pocket-brand-name")
+                    },
+                )
+            ),
+            FTL.Message(
+                id=FTL.Identifier("newtab-label-saved"),
+                value=REPLACE(
+                    "browser/chrome/browser/activity-stream/newtab.properties",
+                    "type_label_pocket",
+                    {
+                        "Pocket": TERM_REFERENCE("pocket-brand-name")
+                    },
+                )
+            ),
+            FTL.Message(
+                id=FTL.Identifier("newtab-empty-section-topstories"),
+                value=REPLACE(
+                    "browser/chrome/browser/activity-stream/newtab.properties",
+                    "topstories_empty_state",
+                    {
+                        "{provider}": VARIABLE_REFERENCE("provider")
+                    },
+                )
+            ),
+        ]
     )
