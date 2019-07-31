45f1fa4ef474a50bccb7737ef4347e1e315e35dd
Kate Hudson <k88hudson@gmail.com>
2019-07-31 14:34:44 -0400

    Bug 1570026 - Part 1. Add Monitor action for snippets
    Resolved conflicts in ASRouterUISurface constructor with not uplifting fe05904ed881d799a6e53d8fbe36976e33fe127c (yet).

--- content-src/asrouter/templates/SimpleBelowSearchSnippet/SimpleBelowSearchSnippet.jsx
+++ content-src/asrouter/templates/SimpleBelowSearchSnippet/SimpleBelowSearchSnippet.jsx
@@ -38,7 +38,7 @@ export class SimpleBelowSearchSnippet extends React.PureComponent {
     ) : null;
   }
 
-  onButtonClick() {
+  async onButtonClick() {
     if (this.props.provider !== "preview") {
       this.props.sendUserActionTelemetry({
         event: "CLICK_BUTTON",
@@ -48,7 +48,7 @@ export class SimpleBelowSearchSnippet extends React.PureComponent {
     const { button_url } = this.props.content;
     // If button_url is defined handle it as OPEN_URL action
     const type = this.props.content.button_action || (button_url && "OPEN_URL");
-    this.props.onAction({
+    await this.props.onAction({
       type,
       data: { args: this.props.content.button_action_args || button_url },
     });
