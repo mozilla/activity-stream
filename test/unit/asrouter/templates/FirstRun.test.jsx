a2e06c2803250a53709fe70b09e207efea9346e7
Kate Hudson <k88hudson@gmail.com>
2019-08-08 12:57:38 -0400

    Bug 1570754 - Add delay to clearing triplets and always block extended

--- test/unit/asrouter/templates/FirstRun.test.jsx
+++ test/unit/asrouter/templates/FirstRun.test.jsx
@@ -44,9 +44,12 @@ describe("<FirstRun>", () => {
   let message;
   let fakeDoc;
   let sandbox;
+  let clock;
+  let onBlockByIdStub;
 
   async function setup() {
     sandbox = sinon.createSandbox();
+    clock = sandbox.useFakeTimers();
     message = await getTestMessage("TRAILHEAD_1");
     fakeDoc = {
       body: document.createElement("body"),
@@ -55,6 +58,7 @@ describe("<FirstRun>", () => {
       getElementById: () => document.createElement("div"),
       activeElement: document.createElement("div"),
     };
+    onBlockByIdStub = sandbox.stub();
 
     sandbox
       .stub(global, "fetch")
@@ -71,6 +75,7 @@ describe("<FirstRun>", () => {
         document={fakeDoc}
         dispatch={() => {}}
         sendUserActionTelemetry={() => {}}
+        onBlockById={onBlockByIdStub}
       />
     );
   }
@@ -198,7 +203,7 @@ describe("<FirstRun>", () => {
     );
   });
 
-  it("should hide triplets when closeTriplets is called", () => {
+  it("should hide triplets when closeTriplets is called and block extended triplets after 500ms", () => {
     // Simulate calling next scene
     wrapper
       .find(Triplets)
@@ -212,5 +217,9 @@ describe("<FirstRun>", () => {
         .hasClass("show"),
       "Show triplet content"
     );
+
+    assert.notCalled(onBlockByIdStub);
+    clock.tick(500);
+    assert.calledWith(onBlockByIdStub, "EXTENDED_TRIPLETS_1");
   });
 });
