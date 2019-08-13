e9215c22f9fe62d0279019986b0fb8c6e7cb94dd
Gavin Lazar Suntop <gavin@gsuntop.com>
2019-08-13 10:52:37 -0700

    lint busting

--- content-src/components/DiscoveryStreamComponents/DSTextPromo/DSTextPromo.jsx
+++ content-src/components/DiscoveryStreamComponents/DSTextPromo/DSTextPromo.jsx
@@ -10,14 +10,14 @@ export class DSTextPromo extends React.PureComponent {
     return (
       <div className="ds-text-promo">
         <img src={this.props.image} alt={this.props.alt_text} />
-        <div class="text">
+        <div className="text">
           <header>
             {`${this.props.header}\u2003`}
             <SafeAnchor className="ds-chevron-link" url={this.props.cta_url}>
               {this.props.cta_text}
             </SafeAnchor>
           </header>
-          <p class="subtitle">{this.props.subtitle}</p>
+          <p className="subtitle">{this.props.subtitle}</p>
         </div>
       </div>
     );
