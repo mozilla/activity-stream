051584813ed3b6aae981c4cf31cd7cd1c3413946
Gavin Lazar Suntop <gavin@gsuntop.com>
2019-08-20 11:13:23 -0700

    fixups

--- content-src/components/DiscoveryStreamComponents/DSTextPromo/DSTextPromo.jsx
+++ content-src/components/DiscoveryStreamComponents/DSTextPromo/DSTextPromo.jsx
@@ -11,12 +11,12 @@ export class DSTextPromo extends React.PureComponent {
       <div className="ds-text-promo">
         <img src={this.props.image} alt={this.props.alt_text} />
         <div className="text">
-          <header>
+          <h3>
             {`${this.props.header}\u2003`}
             <SafeAnchor className="ds-chevron-link" url={this.props.cta_url}>
               {this.props.cta_text}
             </SafeAnchor>
-          </header>
+          </h3>
           <p className="subtitle">{this.props.subtitle}</p>
         </div>
       </div>
