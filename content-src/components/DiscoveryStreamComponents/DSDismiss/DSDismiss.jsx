924c411c696cb736893c3841bb39740abe3dd078
Scott <scott.downe@gmail.com>
2019-08-27 13:20:06 -0400

    TODO note update

--- content-src/components/DiscoveryStreamComponents/DSDismiss/DSDismiss.jsx
+++ content-src/components/DiscoveryStreamComponents/DSDismiss/DSDismiss.jsx
@@ -41,12 +41,7 @@ export class DSDismiss extends React.PureComponent {
   }
 
   render() {
-    // TODO:
-    // This needs an x button that dismisses the campaign_id passed to it.
-    // It also needs a hover state for it and its children.
-    // Somewhere else, we need to filter out anything with a campaign_id that we've blocked.
-    // Consider calling this collection_id.
-    // Right now it is this.props.campaignId
+    // TODO: Styling
     return (
       <div className="ds-dismiss">
         {this.props.children}
