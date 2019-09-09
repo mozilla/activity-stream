4b169cc3823d543d8359ed5c55b6f22a7754b1ac
Scott <scott.downe@gmail.com>
2019-09-09 14:50:52 -0400

    tests

--- test/unit/content-src/components/DiscoveryStreamComponents/DSCard.test.jsx
+++ test/unit/content-src/components/DiscoveryStreamComponents/DSCard.test.jsx
@@ -240,7 +240,7 @@ describe("<DSCard>", () => {
       wrapper.instance().observer = {
         unobserve: sandbox.stub(),
       };
-      wrapper.instance().placholderElement = "element";
+      wrapper.instance().placeholderElement = "element";
 
       wrapper.instance().onSeen([
         {
@@ -258,7 +258,7 @@ describe("<DSCard>", () => {
 
     it("should setup proper placholder ref for isSeen", () => {
       wrapper.instance().setPlaceholderRef("element");
-      assert.equal(wrapper.instance().placholderElement, "element");
+      assert.equal(wrapper.instance().placeholderElement, "element");
     });
 
     it("should setup observer on componentDidMount", () => {
