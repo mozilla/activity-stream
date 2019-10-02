6351c781fd949048766759d35648c94d9ad41de8
gavin lazar suntop <gavin@gsuntop.com>
2019-10-02 13:07:43 -0700

    Bug 1578575 - Ad Privacy Info Modal (#5325)

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,7 +48,8 @@ module.exports = {
         "content-src/components/TopSites/**",
         "content-src/components/MoreRecommendations/MoreRecommendations.jsx",
         "content-src/components/CollapsibleSection/CollapsibleSection.jsx",
-        "content-src/components/DiscoveryStreamComponents/DSEmptyState/DSEmptyState.jsx"
+        "content-src/components/DiscoveryStreamComponents/DSEmptyState/DSEmptyState.jsx",
+        "content-src/components/DiscoveryStreamComponents/DSPrivacyModal/DSPrivacyModal.jsx"
       ],
       "rules": {
         "jsx-a11y/anchor-has-content": 0,
--- content-src/components/DiscoveryStreamBase/DiscoveryStreamBase.jsx
+++ content-src/components/DiscoveryStreamBase/DiscoveryStreamBase.jsx
@@ -8,6 +8,7 @@ import { CollapsibleSection } from "content-src/components/CollapsibleSection/Co
 import { connect } from "react-redux";
 import { DSDismiss } from "content-src/components/DiscoveryStreamComponents/DSDismiss/DSDismiss";
 import { DSMessage } from "content-src/components/DiscoveryStreamComponents/DSMessage/DSMessage";
+import { DSPrivacyModal } from "content-src/components/DiscoveryStreamComponents/DSPrivacyModal/DSPrivacyModal";
 import { DSTextPromo } from "content-src/components/DiscoveryStreamComponents/DSTextPromo/DSTextPromo";
 import { Hero } from "content-src/components/DiscoveryStreamComponents/Hero/Hero";
 import { Highlights } from "content-src/components/DiscoveryStreamComponents/Highlights/Highlights";
@@ -333,6 +334,9 @@ export class _DiscoveryStreamBase extends React.PureComponent {
     // Render a DS-style TopSites then the rest if any in a collapsible section
     return (
       <React.Fragment>
+        {this.props.DiscoveryStream.isPrivacyInfoModalVisible && (
+          <DSPrivacyModal dispatch={this.props.dispatch} />
+        )}
         {topSites &&
           this.renderLayout([
             {
