8c2376a27dc37f5a5bd48d438339688c7d122a89
ScottDowne <scott.downe@gmail.com>
2019-10-08 19:57:29 -0400

    Bug 1579966 - Campaign id blocks (#5315)
    * Bug 1579966 - Campaign id blocks

--- content-src/lib/link-menu-options.js
+++ content-src/lib/link-menu-options.js
@@ -61,12 +61,20 @@ export const LinkMenuOptions = {
     }),
     userEvent: "OPEN_NEW_WINDOW",
   }),
+  // This blocks the url for regular stories,
+  // but also sends a message to DiscoveryStream with campaign_id.
+  // If DiscoveryStream sees this message for a campaign_id
+  // it also blocks it on the campaign_id.
   BlockUrl: (site, index, eventSource) => ({
     id: "newtab-menu-dismiss",
     icon: "dismiss",
     action: ac.AlsoToMain({
       type: at.BLOCK_URL,
-      data: { url: site.open_url || site.url, pocket_id: site.pocket_id },
+      data: {
+        url: site.open_url || site.url,
+        pocket_id: site.pocket_id,
+        ...(site.campaign_id ? { campaign_id: site.campaign_id } : {}),
+      },
     }),
     impression: ac.ImpressionStats({
       source: eventSource,
